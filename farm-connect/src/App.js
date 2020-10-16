fb4c21e86b3c415253cd993ff7dfae68f7f9d276
Alvin Coulson <aaacoulson@gmail.com>
2020-10-16 17:48:15 -0400

    add AppHeader component to the root component

--- farm-connect/src/App.js
+++ farm-connect/src/App.js
@@ -1,56 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import AppHeader from './components/AppHeader/AppHeader';
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-        Welcome to farmConnect!
+        <AppHeader />
       </div>
     );
-  }
-
-  componentDidMount(){
-    const bodyData = {
-      user: {
-            email: "t@sam.com",
-            password: "123"
-          }
-    }
-
-    fetch(`http://localhost:3000/api/v1/signin`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(bodyData)
-    })
-    .then(response => response.json())
-    .then(json => {
-      localStorage.setItem('jwt_token', json.jwt)
-      console.log(json) 
-    })
-
-  //   const bodyData = {
-  //     user:  {
-  //         type: "Farmer",
-  //       first_name: "Troy",
-  //       last_name: "Sam",
-  //       state: "Kansas",
-  //       email: "t@sam.com",
-  //       password: ""
-  //     }
-  //   }
-
-  //   fetch(`http://localhost:3000/api/v1/users`, {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json"
-  //     },
-  //     body: JSON.stringify(bodyData)
-  //   })
-  //   .then(response => response.json())
-  //   .then(json => console.log(json))
   }
 }
 
