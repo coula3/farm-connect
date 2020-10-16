e6349335ac56fd251a7a07578523fff29b366eae
Alvin Coulson <aaacoulson@gmail.com>
2020-10-16 16:56:10 -0400

    draw login route

--- farm-connect-backend/config/routes.rb
+++ farm-connect-backend/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      post '/login', to: "auth#create"
       resources :users
       resources :farmers
       resources :prospects
