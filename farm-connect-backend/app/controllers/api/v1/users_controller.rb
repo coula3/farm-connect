98b6c05b2e60e2d4bb1b02d30472a9a1b2d5112f
Alvin Coulson <aaacoulson@gmail.com>
2020-10-15 23:16:16 -0400

    add show and index actions to UsersController

--- farm-connect-backend/app/controllers/api/v1/users_controller.rb
+++ farm-connect-backend/app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,11 @@
 class Api::V1::UsersController < ApplicationController
+    def show
+        user = User.find_by(id: params[:id])
+        render json: UserSerializer.new(user)
+    end
+
+    def index
+        users = User.all
+        render json: UserSerializer.new(users)
+    end
 end
