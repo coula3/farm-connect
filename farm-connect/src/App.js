67e392d8cd3669ef1194843d381ce2d1deff1231
Alvin Coulson <aaacoulson@gmail.com>
2020-10-16 17:13:10 -0400

    test out JWT with fetch post requests to create and sign in users on App component using componentDidMount method

--- farm-connect/src/App.js
+++ farm-connect/src/App.js
@@ -9,6 +9,49 @@ class App extends Component {
       </div>
     );
   }
+
+  componentDidMount(){
+    const bodyData = {
+      user: {
+            email: "t@sam.com",
+            password: "123"
+          }
+    }
+
+    fetch(`http://localhost:3000/api/v1/signin`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(bodyData)
+    })
+    .then(response => response.json())
+    .then(json => {
+      localStorage.setItem('jwt_token', json.jwt)
+      console.log(json) 
+    })
+
+  //   const bodyData = {
+  //     user:  {
+  //         type: "Farmer",
+  //       first_name: "Troy",
+  //       last_name: "Sam",
+  //       state: "Kansas",
+  //       email: "t@sam.com",
+  //       password: ""
+  //     }
+  //   }
+
+  //   fetch(`http://localhost:3000/api/v1/users`, {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json"
+  //     },
+  //     body: JSON.stringify(bodyData)
+  //   })
+  //   .then(response => response.json())
+  //   .then(json => console.log(json))
+  }
 }
 
 export default App;
