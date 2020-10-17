9758083da894a9fa24b7b17faf8fca41a4015f7d
Alvin Coulson <aaacoulson@gmail.com>
2020-10-16 21:02:07 -0400

    build SignIn container component and add to root component

--- farm-connect/src/App.js
+++ farm-connect/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import AppHeader from './components/AppHeader/AppHeader';
+import SignIn from './containers/SignIn/SignIn';
 
 class App extends Component {
   render(){
     return (
       <div className="App">
         <AppHeader />
+        <SignIn />
       </div>
     );
   }
---
+++ farm-connect/src/containers/SignIn/SignIn.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+
+class SignIn extends Component {
+    state = {
+        user: {
+            email: "",
+            password: ""
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            user: {
+                ...this.state.user,
+                [e.target.name]: e.target.value 
+            }
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        fetch(`http://localhost:3000/api/v1/signin`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(this.state)
+        })
+        .then(response => response.json())
+        .then(json => {
+            localStorage.setItem('jwt_token', json.jwt);
+            console.log(json);
+        })
+
+        this.setState({
+            user: {
+                email: "",
+                password: ""
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div style={{margin: "auto", width: "30%", border: "solid 1px grey", boxShadow: "10px 10px grey", borderRadius: "10px"}}>
+                <form style={{padding: 10, marginBottom: "10px"}} onSubmit={this.handleSubmit}>
+                    <p><input type="text" name="email" placeholder="email" value={this.state.user.email} onChange={this.handleChange} /></p>
+                    <p><input type="password" name="password" placeholder="password" value={this.state.user.password} onChange={this.handleChange} /></p>
+                    <button>Sign In</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SignIn;
