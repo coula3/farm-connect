19d1d64ac84a3bb742093015601cc3d267714b89
Alvin Coulson <aaacoulson@gmail.com>
2020-10-18 10:51:21 -0400

    add action creator 'signInUser'

---
+++ farm-connect/src/actions/userActions.js
@@ -0,0 +1,29 @@
+const signInUser = (payload, ownProps) => {
+    return (dispatch) => {
+        dispatch({type: "LOADING"});
+        fetch(`http://localhost:3000/api/v1/signin`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(payload)
+        })
+        .then(response => response.json())
+        .then(json => {
+            if(json.jwt){
+                dispatch({
+                    type: "LOGIN_SUCCESS",
+                    user: json.user
+                });
+                localStorage.setItem('jwt_token', json.jwt);
+                ownProps.history.push(`/main`);
+                console.log(json.user)
+            } else {
+                dispatch({type: "LOGIN_FAILURE"})
+                console.log(json)
+            }
+        })
+    }
+}
+
+export default signInUser;
