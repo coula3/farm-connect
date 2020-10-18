6c0eb1e3ad53f460436a3e4b85ac47c0c4cd115f
Alvin Coulson <aaacoulson@gmail.com>
2020-10-18 14:21:03 -0400

    build HeadNavBar functional component and add to root component

--- farm-connect/src/App.js
+++ farm-connect/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppHeader from './components/AppHeader/AppHeader';
+import HeadNavBar from './components/HeaderNavBar/HeaderNavBar';
 import Routes from './components/Routes/Routes';
 
 class App extends Component {
@@ -10,6 +11,7 @@ class App extends Component {
       <div className="App">
         <Router>
           <AppHeader />
+          <HeadNavBar />
           <Routes />
         </Router>
       </div>
---
+++ farm-connect/src/components/HeaderNavBar/HeaderNavBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeadNavBar = () => {
+    return (
+        <div style={{marginBottom:"15px"}}>
+            <button>Sign Out</button>
+        </div>
+    )
+}
+
+export default HeadNavBar;
