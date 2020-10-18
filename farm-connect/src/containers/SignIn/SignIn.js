e6ee195351702111238291525342418f954370ed
Alvin Coulson <aaacoulson@gmail.com>
2020-10-18 18:31:05 -0400

    add 'signInUser' action as named import to SignIn component

--- farm-connect/src/containers/SignIn/SignIn.js
+++ farm-connect/src/containers/SignIn/SignIn.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import signInUser from '../../actions/userActions';
 import { connect } from 'react-redux';
+import { signInUser } from '../../actions/userActions';
 
 class SignIn extends Component {
     state = {
