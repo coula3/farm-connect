a2a8f876c5c226c7eea4d8c255c8d781b5d5d918
Alvin Coulson <aaacoulson@gmail.com>
2020-10-17 22:17:19 -0400

    add presence validation to user attributes

--- farm-connect-backend/app/models/user.rb
+++ farm-connect-backend/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-    has_secure_password
+    validates :type, :first_name, :last_name, :date_of_birth, presence: true
     validates :email, uniqueness: { case_sensitive: false }
+    has_secure_password
 end
