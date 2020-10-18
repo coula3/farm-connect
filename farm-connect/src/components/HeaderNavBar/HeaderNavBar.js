76319287dc017879bb14d104022aba7c2c76d63c
Alvin Coulson <aaacoulson@gmail.com>
2020-10-18 17:42:48 -0400

    add signout route to Routes component and add sign out link on HeadNavBar component

--- farm-connect/src/components/HeaderNavBar/HeaderNavBar.js
+++ farm-connect/src/components/HeaderNavBar/HeaderNavBar.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const HeadNavBar = (props) => {
     return (
         <div style={{marginBottom:"15px"}}>
             <span style={{marginRight: "15px"}}>{props.userAttributes.first_name} {props.userAttributes.last_name}</span>
-            <button>Sign Out</button>
+            <Link onClick={props.userSignOut} to="/signout">Sign Out</Link>
         </div>
     )
 }
--- farm-connect/src/components/Routes/Routes.js
+++ farm-connect/src/components/Routes/Routes.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import SignUp from '../../containers/SignUp/SignUp';
 import SignIn from '../../containers/SignIn/SignIn';
-import MainPage from '../MainPage/MainPage';
+import SignOut from '../../components/SignOut/SignOut';
+import MainPage from '../../containers/MainPage/MainPage';
 
 const Routes = () => {
     return (
         <Switch>
             <Route exact path="/" component={SignIn} />
             <Route path="/signup" component={SignUp} />
-            <Route path="/main" render={(props) => <MainPage props={props} />} /> }
+            <Route path="/signout" component={SignOut} />
+            <Route path="/main" render={routerProps => <MainPage {...routerProps} />} /> }
         </Switch>
     )
 }
