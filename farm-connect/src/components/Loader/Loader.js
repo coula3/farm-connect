3c3fe6f96baea7963f709d4f2f47c2cb8ee923e8
Alvin Coulson <aaacoulson@gmail.com>
2020-10-18 17:31:37 -0400

    build a Loader functional component

---
+++ farm-connect/src/components/Loader/Loader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import loaderSrc from '../../assets/loader.gif'
+
+const Loader = () => (
+    <div>
+        <img alt="loader" src={loaderSrc} style={{width: 75}} />
+    </div>
+)
+
+export default Loader;
