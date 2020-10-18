cbee60193cac592809f93606ca453ba66bac49bd
Alvin Coulson <aaacoulson@gmail.com>
2020-10-17 22:15:54 -0400

    add MainPage functional component and add associated route

---
+++ farm-connect/src/components/MainPage/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainPage = (props) => {
+    return (
+        <div>
+            Main Page
+            {console.log(props)}
+        </div>
+    )
+}
+
+export default MainPage;
--- farm-connect/src/components/Routes/Routes.js
+++ farm-connect/src/components/Routes/Routes.js
@@ -2,15 +2,16 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import SignUp from '../../containers/SignUp/SignUp';
 import SignIn from '../../containers/SignIn/SignIn';
+import MainPage from '../MainPage/MainPage';
 
 const Routes = () => {
     return (
         <Switch>
             <Route exact path="/" component={SignIn} />
             <Route path="/signup" component={SignUp} />
+            <Route path="/main" render={(props) => <MainPage props={props} />} /> }
         </Switch>
     )
-
 }
 
 export default Routes;
