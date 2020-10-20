e104a0036750171b8c3ebed299f68ac84eb4a5d4
Alvin Coulson <aaacoulson@gmail.com>
2020-10-20 19:03:37 -0400

    add interests attribute to listing serializer

--- farm-connect-backend/app/serializers/listing_serializer.rb
+++ farm-connect-backend/app/serializers/listing_serializer.rb
@@ -1,4 +1,4 @@
 class ListingSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :user_id, :commodity_id, :list_date, :est_availability, :available, :measure, :quantity, :user, :commodity
+  attributes :user_id, :commodity_id, :list_date, :est_availability, :available, :measure, :quantity, :user, :commodity, :interests
 end
