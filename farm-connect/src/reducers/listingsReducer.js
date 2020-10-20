1aea2eb1f72c0b67fc9a1b50408268618d964799
Alvin Coulson <aaacoulson@gmail.com>
2020-10-20 13:17:22 -0400

    add listingsReducer

---
+++ farm-connect/src/reducers/listingsReducer.js
@@ -0,0 +1,26 @@
+const initialState = {
+    isLoading: false,
+    listings: []
+}
+
+const manageListings = (state = initialState, action) => {
+    switch (action.type) {
+        case "LOADING":
+            return {
+                ...state,
+                isLoading: true
+            };
+            
+        case "FETCH_LISTINGS":
+            return {
+                ...state,
+                listings: action.listings,
+                isLoading: false
+            }
+    
+        default:
+            return state
+    }
+}
+
+export default manageListings;
