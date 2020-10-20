b2f440bd626aeec428ae065b3b4faf417d516661
Alvin Coulson <aaacoulson@gmail.com>
2020-10-20 16:14:52 -0400

    add seed data to listings table

--- farm-connect-backend/db/seeds.rb
+++ farm-connect-backend/db/seeds.rb
@@ -1,26 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# require 'csv'
 
+# #FARMER SEED DATA
 # Farmer.create(first_name: "Jeff", last_name: "Druff", email: "j@druff.com", state: "North Carolina")
 # Farmer.create(first_name: "Lisa", last_name: "Vikes", email: "l@vikes.com", state: "Iowa")
 
+# #PROSPECT SEED DATA
 # Prospect.create(first_name: "Sue", last_name: "May", email: "s@may.com", state: "Indiana")
 # Prospect.create(first_name: "Ann", last_name: "Moe", email: "a@moe.com", state: "Kansas")
 # puts "Completed!"
 
-require 'csv'
+# #COMMODITY SEED DATA
+# FILE_PATH = "/Users/acoulson/Development/code/farm-connect/farm-connect-backend/db"
 
-FILE_PATH = "/Users/acoulson/Development/code/farm-connect/farm-connect-backend/db"
+# items = []
 
-items = []
+# CSV.foreach("#{FILE_PATH}/commodity.csv", headers: true) do |row|
+#     items << row.to_h
+# end
 
-CSV.foreach("#{FILE_PATH}/commodity.csv", headers: true) do |row|
-    items << row.to_h
-end
+# Commodity.import(items)
 
-Commodity.import(items)
+# #LISTING SEED DATA
+farmers = User.where(type: "Farmer")
+wheat = Commodity.find_by(name: "Wheat")
+yam = Commodity.find_by(name: "Yam")
+tomatoes = Commodity.find_by(name: "Tomatoes")
+
+listing1 = farmers.first.listings.create(commodity_id: wheat.id, list_date: Time.now)
+listing2 = farmers.second.listings.create(commodity_id: yam.id, list_date: Time.now)
+listing3 = farmers.second.listings.create(commodity_id: wheat.id, list_date: Time.now)
+listing4 = farmers.last.listings.create(commodity_id: tomatoes.id, list_date: Time.now)
