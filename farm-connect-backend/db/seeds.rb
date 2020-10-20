483a7cf5e73511a1f9573c0723f448d99c3f45c2
Alvin Coulson <aaacoulson@gmail.com>
2020-10-20 01:43:36 -0400

    populate commodities table from CSV file

--- farm-connect-backend/db/seeds.rb
+++ farm-connect-backend/db/seeds.rb
@@ -6,9 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Farmer.create(first_name: "Jeff", last_name: "Druff", email: "j@druff.com", state: "North Carolina")
-Farmer.create(first_name: "Lisa", last_name: "Vikes", email: "l@vikes.com", state: "Iowa")
+# Farmer.create(first_name: "Jeff", last_name: "Druff", email: "j@druff.com", state: "North Carolina")
+# Farmer.create(first_name: "Lisa", last_name: "Vikes", email: "l@vikes.com", state: "Iowa")
 
-Prospect.create(first_name: "Sue", last_name: "May", email: "s@may.com", state: "Indiana")
-Prospect.create(first_name: "Ann", last_name: "Moe", email: "a@moe.com", state: "Kansas")
-puts "Completed!"
+# Prospect.create(first_name: "Sue", last_name: "May", email: "s@may.com", state: "Indiana")
+# Prospect.create(first_name: "Ann", last_name: "Moe", email: "a@moe.com", state: "Kansas")
+# puts "Completed!"
+
+require 'csv'
+
+FILE_PATH = "/Users/acoulson/Development/code/farm-connect/farm-connect-backend/db"
+
+items = []
+
+CSV.foreach("#{FILE_PATH}/commodity.csv", headers: true) do |row|
+    items << row.to_h
+end
+
+Commodity.import(items)
