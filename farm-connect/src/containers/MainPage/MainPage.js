58df91fe882d1d58c387e03825f000059966aae7
Alvin Coulson <aaacoulson@gmail.com>
2020-10-21 13:05:48 -0400

    add commodities to the Redux store

--- farm-connect/src/containers/MainPage/MainPage.js
+++ farm-connect/src/containers/MainPage/MainPage.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { fetchListings } from '../../actions/listingsActions';
 import Listings from '../../components/Listings/Listings';
+import { fetchCommodities } from '../../actions/commoditiesActions';
 
 class MainPage extends React.Component {
     componentDidMount(){
        this.props.fetchListings();
+       this.props.fetchCommodities();
     }
 
     render (){
@@ -20,13 +22,15 @@ class MainPage extends React.Component {
 const mapStateToProps = (state) => {
     return {
         isLoading: state.listings.isLoading,
-        listings: state.listings.listings
+        listings: state.listings.listings,
+        commodities: state.commodities.commodities
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        fetchListings: () => dispatch(fetchListings())
+        fetchListings: () => dispatch(fetchListings()),
+        fetchCommodities: () => dispatch(fetchCommodities())
     }
 }
 
