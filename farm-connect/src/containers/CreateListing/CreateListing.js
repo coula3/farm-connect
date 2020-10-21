a21730773709a3277db3ef1ba975f450e68ec7c0
Alvin Coulson <aaacoulson@gmail.com>
2020-10-21 13:37:18 -0400

    add input submit element and wrap with other input elements in a form element

--- farm-connect/src/containers/CreateListing/CreateListing.js
+++ farm-connect/src/containers/CreateListing/CreateListing.js
@@ -29,40 +29,43 @@ class CreateListing extends Component {
         return (
             <div style={{width: "60%", display: "inline", float: "left"}}>
                 <h3>Create Listing</h3>
-                <p>
-                    <label>List Date </label> 
-                    <input name="listDate" type="date" value={this.state.listDate} onChange={this.handleChange} />
-                </p>
-                <p>
-                    <label>Commodity </label> 
-                    <select name="commodity" id="commodity-select" value={this.state.commodity} onChange={this.handleChange}>
-                        { commoditiesList.map((commodity, idx) =>
-                            (<option key={idx} value={commodity}>{commodity}</option>))
-                        }
-                    </select>
-                </p>
-                <p>
-                    <label>Estimated Availability </label>
-                    <input name="estAvailability" type="date" value={this.state.estAvailability} onChange={this.handleChange} />
-                </p>
-                <p>
-                    <label>Measure </label>
-                    <select name="measure" value={this.state.measure} onChange={this.handleChange}>
-                       {measuresList.map((measure, idx) =>
-                       (<option key={idx} value={measure}>{measure}</option>))}
-                    </select>
-                </p> 
-                <p>
-                    <label>Quantity </label>
-                    <input name="quantity" type="number" min="0" value={this.state.quantity} onChange={this.handleChange} />
-                </p>
-                <p>
-                    <label>Available </label> 
-                    <select name="available" id="available-select" value={this.state.available} onChange={this.handleChange}>
-                        <option value="No">No</option>
-                        <option value="Yes">Yes</option>
-                    </select>
-                </p>
+                <form>
+                    <p>
+                        <label>List Date </label> 
+                        <input name="listDate" type="date" value={this.state.listDate} onChange={this.handleChange} />
+                    </p>
+                    <p>
+                        <label>Commodity </label> 
+                        <select name="commodity" id="commodity-select" value={this.state.commodity} onChange={this.handleChange}>
+                            { commoditiesList.map((commodity, idx) =>
+                                (<option key={idx} value={commodity}>{commodity}</option>))
+                            }
+                        </select>
+                    </p>
+                    <p>
+                        <label>Estimated Availability </label>
+                        <input name="estAvailability" type="date" value={this.state.estAvailability} onChange={this.handleChange} />
+                    </p>
+                    <p>
+                        <label>Quantity </label>
+                        <input name="quantity" type="number" min="0" value={this.state.quantity} onChange={this.handleChange} />
+                    </p>
+                    <p>
+                        <label>Measure </label>
+                        <select name="measure" value={this.state.measure} onChange={this.handleChange}>
+                        {measuresList.map((measure, idx) =>
+                        (<option key={idx} value={measure}>{measure}</option>))}
+                        </select>
+                    </p> 
+                    <p>
+                        <label>Available </label> 
+                        <select name="available" id="available-select" value={this.state.available} onChange={this.handleChange}>
+                            <option value="No">No</option>
+                            <option value="Yes">Yes</option>
+                        </select>
+                    </p>
+                    <input type="submit" /> 
+                </form>
             </div>
         )
     }
