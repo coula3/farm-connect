b72efd462851af42a66ebfb6044b4e50c085bfe4
Alvin Coulson <aaacoulson@gmail.com>
2020-10-21 12:57:30 -0400

    add action creator 'FETCH_COMMODITIES'

---
+++ farm-connect/src/actions/commoditiesActions.js
@@ -0,0 +1,19 @@
+export const fetchCommodities = () => {
+    return (dispatch) => {
+        dispatch({type: "LOADING_COMMODITIES"});
+        fetch(`http://localhost:3000/api/v1/commodities`, {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('jwt_token')}`
+            }
+        })
+        .then(response => response.json())
+        .then(json => {
+            dispatch({
+                type: "FETCH_COMMODITIES",
+                commodities: json.data
+            });
+            console.log(json)
+        })
+    }
+}
