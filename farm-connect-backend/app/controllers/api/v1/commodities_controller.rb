c6431b0d400a7a9732d372c1f041b84e64d3b5eb
Alvin Coulson <aaacoulson@gmail.com>
2020-10-21 12:35:49 -0400

    add index action to commodities controller

--- farm-connect-backend/app/controllers/api/v1/commodities_controller.rb
+++ farm-connect-backend/app/controllers/api/v1/commodities_controller.rb
@@ -1,2 +1,6 @@
 class Api::V1::CommoditiesController < ApplicationController
+    def index
+        commodities = Commodity.all
+        render json: CommoditySerializer.new(commodities)
+    end
 end
