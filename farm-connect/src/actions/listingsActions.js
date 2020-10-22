0b35afbea247527599f1879d6df4c95e40eed478
Alvin Coulson <aaacoulson@gmail.com>
2020-10-21 22:19:48 -0400

    add action creator 'CREATE_NEW_LISTING'

--- farm-connect/src/actions/listingsActions.js
+++ farm-connect/src/actions/listingsActions.js
@@ -17,3 +17,25 @@ export const fetchListings = () => {
         });
     }
 }
+
+export const createListing = (payload) => {
+    return (dispatch) => {
+        dispatch({type: "LOADING_NEW_LISTING"})
+        fetch(`http://localhost:3000/api/v1/listings`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem('jwt_token')}`
+            },
+            body: JSON.stringify(payload)
+        })
+        .then(response => response.json())
+        .then(json => {
+            dispatch({
+                type: "CREATE_NEW_LISTING",
+                listing: json.data
+            });
+            console.log(json)
+        })
+    }
+}
