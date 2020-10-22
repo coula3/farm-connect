81e2fd25724af52babe18d15e8995fd7167ccf0a
Alvin Coulson <aaacoulson@gmail.com>
2020-10-21 22:14:24 -0400

    build create action and listing_params on ListingsController

--- farm-connect-backend/app/controllers/api/v1/listings_controller.rb
+++ farm-connect-backend/app/controllers/api/v1/listings_controller.rb
@@ -3,4 +3,18 @@ class Api::V1::ListingsController < ApplicationController
         listings = Listing.all
         render json: ListingSerializer.new(listings)
     end
+
+    def create
+        user = User.find_by(id: listing_params[:userId])
+        commodity = Commodity.find_by(name: listing_params[:commodity])
+        available = listing_params[:available] == "Yes" ? true : false
+
+        listing = user.listings.create(commodity_id: commodity.id, list_date: listing_params[:listDate], est_availability: listing_params[:estAvailability], measure: listing_params[:measure], quantity: listing_params[:quantity], available: available)
+
+        render json: ListingSerializer.new(listing)
+    end
+
+    def listing_params
+        params.require(:listing).permit(:listDate, :commodity, :estAvailability, :measure, :quantity, :available, :userId)
+    end
 end
