0aca5f0af2070fffd25dc58c73a6755d0e3bdcc2
Alvin Coulson <aaacoulson@gmail.com>
2020-10-22 01:46:31 -0400

    update listings controller and serializer with new supp_info attribute

--- farm-connect-backend/app/controllers/api/v1/listings_controller.rb
+++ farm-connect-backend/app/controllers/api/v1/listings_controller.rb
@@ -9,12 +9,12 @@ class Api::V1::ListingsController < ApplicationController
         commodity = Commodity.find_by(name: listing_params[:commodity])
         available = listing_params[:available] == "Yes" ? true : false
 
-        listing = user.listings.create(commodity_id: commodity.id, list_date: listing_params[:listDate], est_availability: listing_params[:estAvailability], measure: listing_params[:measure], quantity: listing_params[:quantity], available: available)
+        listing = user.listings.create(commodity_id: commodity.id, list_date: listing_params[:listDate], est_availability: listing_params[:estAvailability], measure: listing_params[:measure], quantity: listing_params[:quantity], available: available, supp_info: listing_params[:suppInfo])
 
         render json: ListingSerializer.new(listing)
     end
 
     def listing_params
-        params.require(:listing).permit(:listDate, :commodity, :estAvailability, :measure, :quantity, :available, :userId)
+        params.require(:listing).permit(:listDate, :commodity, :estAvailability, :measure, :quantity, :available, :suppInfo, :userId)
     end
 end
--- farm-connect-backend/app/serializers/listing_serializer.rb
+++ farm-connect-backend/app/serializers/listing_serializer.rb
@@ -1,4 +1,4 @@
 class ListingSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :user_id, :commodity_id, :list_date, :est_availability, :available, :measure, :quantity, :user, :commodity, :interests
+  attributes :user_id, :commodity_id, :list_date, :est_availability, :available, :measure, :quantity, :supp_info, :user, :commodity, :interests
 end
