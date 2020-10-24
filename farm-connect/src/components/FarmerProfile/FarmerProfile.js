79b23ca1c503e80c60e3f362a1c7107d296b8bb3
Alvin Coulson <aaacoulson@gmail.com>
2020-10-23 23:42:59 -0400

    build basic structure of FarmerProfile component

---
+++ farm-connect/src/components/FarmerProfile/FarmerProfile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FarmerProfile = () => {
+    return (
+        <div>
+            Hello from Farmer Profile Component!
+        </div>
+    )
+}
+
+export default FarmerProfile;
