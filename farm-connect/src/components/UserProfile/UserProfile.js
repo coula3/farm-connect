29740b6f4a506532beb2f74cbddba95e18da1aaf
Alvin Coulson <aaacoulson@gmail.com>
2020-10-24 23:24:06 -0400

    build UserProfile component

---
+++ farm-connect/src/components/UserProfile/UserProfile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const UserProfile = (props) => {
+    return (
+        <div style={{width: "60%", display: "inline", float: "left"}}>
+            <p><label>ID: </label>{props.userId}</p>
+            <p><label>eMail: </label>{props.userAttributes.email}</p>
+            <br />
+            <p><label>Joined: </label>{props.userAttributes.created_at.slice(0, 10)}</p>
+        </div>
+    )
+}
+
+export default UserProfile;
