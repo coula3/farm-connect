7d431d43282fa48c163d51ab8af6934d930f5f29
Alvin Coulson <aaacoulson@gmail.com>
2020-10-24 23:49:30 -0400

    build basic structure of EditListing container

---
+++ farm-connect/src/containers/EditListing/EditListing.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class EditListing extends Component {
+    render(){
+        return (
+            <div style={{width: "60%", display: "inline", float: "left"}}>
+                Edit Listing Component
+            </div>
+        )
+    }
+}
+
+export default EditListing;
