a12f24e4b294d29dcbdd774e72079626df159cb3
Alvin Coulson <aaacoulson@gmail.com>
2020-10-24 21:30:10 -0400

    add Google font to app name and add app slogan in AppHeader component

--- farm-connect/src/components/AppHeader/AppHeader.js
+++ farm-connect/src/components/AppHeader/AppHeader.js
@@ -3,9 +3,10 @@ import logoSrc from '../../assets/farm-connect-logo.png'
 
 const AppHeader = () => {
     return(
-        <div style={{marginTop: 15}}>
+        <div style={{margin: "15px 0px 40px 0px"}}>
             <img alt="Farm Connect Logo" src={logoSrc} style={{height: "5%", width: "5%"}}/>
-            <h1>Farm Connect</h1>
+            <div style={{color: "#3a5f0b", fontSize: 50, fontFamily: 'Piedra'}}>farmConnect</div>
+            <p>changing lives through connects</p>
         </div>
     )
 }
