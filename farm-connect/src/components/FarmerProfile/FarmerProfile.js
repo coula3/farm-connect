18013796cd56dc4c9f9713c4f815521cae950008
Alvin Coulson <aaacoulson@gmail.com>
2020-10-24 20:23:55 -0400

    render farmer profile data on FarmerProfile component

--- farm-connect/src/components/FarmerProfile/FarmerProfile.js
+++ farm-connect/src/components/FarmerProfile/FarmerProfile.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import Loader from '../Loader/Loader';
 
-const FarmerProfile = () => {
+const FarmerProfile = (props) => {
     return (
-        <div>
-            Hello from Farmer Profile Component!
+        <div style={{width: "60%", display: "inline", float: "left"}}>
+            { props.isLoadingFarmer ?
+                <Loader /> :
+                <div>
+                    Farmer
+                    <p><label>Farmer ID: </label>{props.farmer.id}</p>
+                    <p><label>Name: </label>{props.farmer.attributes.first_name} {props.farmer.attributes.last_name}</p>
+                    <p><label>eMail: </label>{props.farmer.attributes.email}</p>
+                    <br />
+                    <p><label>Joined: </label>{props.farmer.attributes.created_at.slice(0, 10)}</p>
+                </div>
+            }
         </div>
     )
 }
