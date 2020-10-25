35cee702045014f399f1cb36d7c630147d418e74
Alvin Coulson <aaacoulson@gmail.com>
2020-10-24 20:31:18 -0400

    add link to 'Home' on SignOut component

--- farm-connect/src/components/SignOut/SignOut.js
+++ farm-connect/src/components/SignOut/SignOut.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const SignOut = () => {
     return (
         <div>
             You have successfully signed out!
+            <br />
+            <br />
+            <Link to="/">Home</Link>
         </div>
     )
 }
