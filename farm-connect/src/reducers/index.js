9c0340857637d4d041fc0d93d61ac8db44e0f93f
Alvin Coulson <aaacoulson@gmail.com>
2020-10-26 17:36:03 -0400

    add prospects reducer to combined reducer

--- farm-connect/src/reducers/index.js
+++ farm-connect/src/reducers/index.js
@@ -3,12 +3,14 @@ import userReducer from './userReducer';
 import listingsReducer from './listingsReducer';
 import commoditiesReducer from './commoditiesReducer';
 import farmersReducer from './farmersReducer';
+import prospectsReducer from './prospectsReducer';
 
 const rootReducer = combineReducers({
     currentUser: userReducer,
     listings: listingsReducer,
     commodities: commoditiesReducer,
-    farmers: farmersReducer
+    farmers: farmersReducer,
+    prospects: prospectsReducer
 })
 
 export default rootReducer;
