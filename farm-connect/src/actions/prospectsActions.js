f758a710377eba48618129c05525e7606e053ca0
Alvin Coulson <aaacoulson@gmail.com>
2020-10-26 22:36:38 -0400

    re-tab 'fetchProspects'

--- farm-connect/src/actions/prospectsActions.js
+++ farm-connect/src/actions/prospectsActions.js
@@ -1,21 +1,21 @@
 export const fetchProspects = () => {
-    return (dispatch) => {
-        dispatch({type: "LOADING_PROSPECTS"});
-        fetch(`http://localhost:3000/api/v1/prospects`, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${localStorage.getItem('jwt_token')}`
-          }
-        })
-        .then(response => response.json())
-        .then(json => {
-            dispatch({
-                type: "FETCH_PROSPECTS",
-                prospects: json.data
-            })
-        })
-    }
+  return (dispatch) => {
+    dispatch({type: "LOADING_PROSPECTS"});
+    fetch(`http://localhost:3000/api/v1/prospects`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem('jwt_token')}`
+      }
+    })
+    .then(response => response.json())
+    .then(json => {
+      dispatch({
+        type: "FETCH_PROSPECTS",
+        prospects: json.data
+      })
+    })
+  }
 }
 
 export const fetchProspect = (id) => {
