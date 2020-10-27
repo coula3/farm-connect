74a29a1eccd55094d7398bd8fcc777a98da54c61
Alvin Coulson <aaacoulson@gmail.com>
2020-10-26 22:00:27 -0400

    build show action on ProspectsController

--- farm-connect-backend/app/controllers/api/v1/prospects_controller.rb
+++ farm-connect-backend/app/controllers/api/v1/prospects_controller.rb
@@ -1,4 +1,9 @@
 class Api::V1::ProspectsController < ApplicationController
+    def show
+        prospect = Prospect.find_by(id: params[:id])
+        render json: ProspectSerializer.new(prospect)
+    end
+
     def index
         prospects = Prospect.all
         render json: ProspectSerializer.new(prospects)
