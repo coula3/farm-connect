93d42279ed550401d6eaea2f10c16e700ace1729
Alvin Coulson <aaacoulson@gmail.com>
2020-10-27 19:08:46 -0400

    migrate connections

--- farm-connect-backend/db/schema.rb
+++ farm-connect-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_22_053236) do
+ActiveRecord::Schema.define(version: 2020_10_27_224052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,11 +21,11 @@ ActiveRecord::Schema.define(version: 2020_10_22_053236) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "connects", force: :cascade do |t|
-    t.bigint "user_id", null: false
+  create_table "connections", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "connect_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_connects_on_user_id"
   end
 
   create_table "interests", force: :cascade do |t|
@@ -64,7 +64,6 @@ ActiveRecord::Schema.define(version: 2020_10_22_053236) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "connects", "users"
   add_foreign_key "interests", "listings"
   add_foreign_key "interests", "users"
   add_foreign_key "listings", "commodities"
