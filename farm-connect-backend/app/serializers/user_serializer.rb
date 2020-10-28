abc075320601b0616bab616aa8d540aec038da10
Alvin Coulson <aaacoulson@gmail.com>
2020-10-27 21:13:01 -0400

    add 'inverse_connects' attribute to UserSerializer

--- farm-connect-backend/app/serializers/user_serializer.rb
+++ farm-connect-backend/app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :type, :first_name, :last_name, :date_of_birth, :state, :email, :created_at, :listings, :interests, :connects
+  attributes :type, :first_name, :last_name, :date_of_birth, :state, :email, :created_at, :listings, :interests, :connects, :inverse_connects
 end
