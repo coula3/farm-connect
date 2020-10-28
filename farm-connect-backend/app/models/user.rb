decab71f0936256e766033e9e2a8b9163118895d
Alvin Coulson <aaacoulson@gmail.com>
2020-10-27 21:11:42 -0400

    define the inverse of the connect relationship

--- farm-connect-backend/app/models/user.rb
+++ farm-connect-backend/app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
     has_many :connections
     has_many :connects, through: :connections
+
+    has_many :inverse_connections, class_name: :Connection, foreign_key: :connect_id
+    has_many :inverse_connects, through: :inverse_connections, source: :user
+
     has_many :interests
     has_many :listings
+
     validates :type, :first_name, :last_name, :date_of_birth, presence: true
     validates :email, uniqueness: { case_sensitive: false }
     has_secure_password
