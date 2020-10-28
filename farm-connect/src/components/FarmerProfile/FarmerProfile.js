2cb707ec0dc314d92f96915cb6f2cf56efa73964
Alvin Coulson <aaacoulson@gmail.com>
2020-10-28 10:51:09 -0400

    add button to connect and unconnect on FarmerProfile

--- farm-connect/src/components/FarmerProfile/FarmerProfile.js
+++ farm-connect/src/components/FarmerProfile/FarmerProfile.js
@@ -2,12 +2,22 @@ import React from 'react';
 import Loader from '../Loader/Loader';
 
 const FarmerProfile = (props) => {
+    const isConnected = props.userAttributes.connects.find(connect => {
+        return connect.id === parseInt(props.farmer.id);
+    })
+
     return (
         <div style={{width: "60%", display: "inline", float: "left"}}>
             { props.isLoadingFarmer ?
                 <Loader /> :
                 <div>
+                    <br />
                     Farmer
+                    { !isConnected ?
+                        <div style={{paddingTop:15}}><button>Connect</button></div> :
+                        <div style={{paddingTop:15}}><button>Unconnect</button></div>
+                    }
+
                     <p><label>Farmer ID: </label>{props.farmer.id}</p>
                     <p><label>Name: </label>{props.farmer.attributes.first_name} {props.farmer.attributes.last_name}</p>
                     <p><label>eMail: </label>{props.farmer.attributes.email}</p>
