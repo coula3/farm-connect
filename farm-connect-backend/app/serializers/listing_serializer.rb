66263282a364b88589e448eafa1b7b8cf7aa6d80
Alvin Coulson <aaacoulson@gmail.com>
2020-10-29 01:32:13 -0400

    add 'close_listing' to ListingSerializer

--- farm-connect-backend/app/serializers/listing_serializer.rb
+++ farm-connect-backend/app/serializers/listing_serializer.rb
@@ -1,4 +1,4 @@
 class ListingSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :user_id, :commodity_id, :list_date, :est_availability, :available, :measure, :quantity, :supp_info, :user, :commodity, :interests
+  attributes :user_id, :commodity_id, :list_date, :est_availability, :available, :measure, :quantity, :supp_info, :close_listing, :user, :commodity, :interests
 end
