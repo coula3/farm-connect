d4b6f58163b99cb842ca6b5714d5651a6b462a3e
Alvin Coulson <aaacoulson@gmail.com>
2020-10-30 03:29:22 -0400

    handle change on EditUser

--- farm-connect/src/containers/EditUser/EditUser.js
+++ farm-connect/src/containers/EditUser/EditUser.js
@@ -11,14 +11,23 @@ class EditUser extends Component {
         }
     }
 
+    handleChange = (e) => {
+        this.setState({
+            ...this.state.user,
+            user: {
+                [e.target.name]: e.target.value
+            }
+        })
+    }
+
     render(){
         return (
             <div style={{width: "60%", display: "inline", float: "left"}}>
                 <form style={{padding: 10, marginBottom: "5px"}} >
-                    <p><input type="text" name="firstName" value={this.props.userAttributes.first_name} /></p>
-                    <p><input type="text" name="lastName" value={this.props.userAttributes.last_name} /></p>
-                    <p><input type="date" name="dateOfBirth" value={this.props.userAttributes.date_of_birth.slice(0, 10)} /></p>
-                    <p><input type="text" name="email" value={this.props.userAttributes.email} /></p>
+                    <p><input type="text" name="firstName" value={this.state.user.firstName ? this.state.user.firstName : this.props.userAttributes.first_name} onChange={this.handleChange} /></p>
+                    <p><input type="text" name="lastName" value={this.state.user.lastName ? this.state.user.lastName : this.props.userAttributes.last_name} onChange={this.handleChange} /></p>
+                    <p><input type="date" name="dateOfBirth" value={this.state.user.dateOfBirth ? this.state.user.dateOfBirth : this.props.userAttributes.date_of_birth.slice(0, 10)}onChange={this.handleChange}  /></p>
+                    <p><input type="text" name="email" value={this.state.user.email ? this.state.user.email : this.props.userAttributes.email} onChange={this.handleChange} /></p>
                     <br />
                     <button>Update</button>
                 </form>
