3344d460ecc34ed67628d04e16255a8e7ef7fa87
Alvin Coulson <aaacoulson@gmail.com>
2020-10-30 13:03:28 -0400

    add 'Date of Birth' to CurrentUser

--- farm-connect/src/components/CurrentUser/CurrentUser.js
+++ farm-connect/src/components/CurrentUser/CurrentUser.js
@@ -4,6 +4,7 @@ const CurrentUser = (props) => {
     return (
         <div style={{width: "60%", display: "inline", float: "left"}}>
             <p><label>ID: </label>{props.userId}</p>
+            <p><label>Date of Birth: </label>{props.userAttributes.date_of_birth.slice(0, 10)}</p>
             <p><label>eMail: </label>{props.userAttributes.email}</p>
             <br />
             <p><label>Joined: </label>{props.userAttributes.created_at.slice(0, 10)}</p>
