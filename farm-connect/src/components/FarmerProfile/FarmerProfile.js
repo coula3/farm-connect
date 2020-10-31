c4c89e1a2a00cfd76a380fba13e0c16cf6c98088
Alvin Coulson <aaacoulson@gmail.com>
2020-10-30 22:24:10 -0400

    render farmer photo or anonymous avatar on FarmerProfile

--- farm-connect/src/components/FarmerProfile/FarmerProfile.js
+++ farm-connect/src/components/FarmerProfile/FarmerProfile.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Loader from '../Loader/Loader';
+import avatar from '../../assets/avatar.png';
 
 const FarmerProfile = (props) => {
     const consolidatedConnects = [ ...props.userAttributes.connects, ...props.userAttributes.inverse_connects ];
     const isConnected = consolidatedConnects.find(connect => {
         return connect.id === parseInt(props.farmer.id);
     });
+
+    const farmerPhoto = (farmerPhoto) => {
+       return `http://localhost:3000/${farmerPhoto}`;
+    }
 
     const isCurrentUser = (farmerId) => {
         return props.userId === farmerId;
@@ -24,8 +29,7 @@ const FarmerProfile = (props) => {
             { props.isLoadingFarmer ?
                 <Loader /> :
                 <div>
-                    <br />
-                    Farmer
+                    <h3>Farmer</h3>
                     {!isCurrentUser(props.farmer.id) ?
                         !isConnected ?
                             <div style={{paddingTop:15}}><button style={{backgroundColor:"#3a5f0b", color:"#FFFFFF"}} onClick={(e) => connectUnconnectUsers(e, props.userId, props.farmer.id)}>Connect</button></div> :
@@ -33,7 +37,13 @@ const FarmerProfile = (props) => {
                     :
                         null
                     }
-
+                    <br />
+                    <div>
+                        {props.farmerPhoto ?
+                            <img src={farmerPhoto(props.farmerPhoto)} alt="user avatar" style={{height:350, width:300}} /> :
+                            <img src={avatar} alt="anonymous avatar" style={{height:100, width:100}} />
+                        }
+                    </div>
                     <p><label>Farmer ID: </label>{props.farmer.id}</p>
                     <p><label>Name: </label>{props.farmer.attributes.first_name} {props.farmer.attributes.last_name}</p>
                     <p><label>eMail: </label>{props.farmer.attributes.email}</p>
