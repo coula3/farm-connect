dd080d2f7dddd3185829891365e92077208d471c
Alvin Coulson <aaacoulson@gmail.com>
2020-10-30 20:05:57 -0400

    source user photo on AuthController for FE

--- farm-connect-backend/app/controllers/api/v1/auth_controller.rb
+++ farm-connect-backend/app/controllers/api/v1/auth_controller.rb
@@ -4,8 +4,9 @@ class Api::V1::AuthController < ApplicationController
     def create
         @user = User.find_by(email: login_params[:email])
         if @user && @user.authenticate(login_params[:password])
+            @photo = rails_blob_path(@user.photo) if @user.photo.attached?
             @token = encode_token({user_id: @user.id})
-            render json: {user: UserSerializer.new(@user), jwt: @token}, status: :accepted
+            render json: {user: UserSerializer.new(@user), photo: @photo, jwt: @token}, status: :accepted
         else
             render json: {message: "Invalid username or password"}, status: :unauthorized
         end
