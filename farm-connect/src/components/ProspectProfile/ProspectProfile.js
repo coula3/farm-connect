84b59a2a8b908bb406f2585698c51dc109bdc931
Alvin Coulson <aaacoulson@gmail.com>
2020-11-01 13:26:00 -0500

    add prospect's image to ProspectProfile

--- farm-connect/src/components/ProspectProfile/ProspectProfile.js
+++ farm-connect/src/components/ProspectProfile/ProspectProfile.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Loader from '../Loader/Loader';
+import avatar from '../../assets/avatar.png';
 
 const ProspectProfile = (props) => {
     const consolidatedConnects = [ ...props.userAttributes.connects, ...props.userAttributes.inverse_connects ];
@@ -7,6 +8,10 @@ const ProspectProfile = (props) => {
         return consolidatedConnects.find(connect => {
             return connect.id === parseInt(prospectId);
         });
+    }
+
+    const prospectPhoto = (prospectPhoto) => {
+        return `http://localhost:3000/${prospectPhoto}`;
     }
 
     const connectUnconnectUsers = (e, userId, prospectId) => {
@@ -27,6 +32,13 @@ const ProspectProfile = (props) => {
                         <div style={{paddingTop:15}}><button style={{backgroundColor:"#3a5f0b", color:"#FFFFFF"}} onClick={(e) => connectUnconnectUsers(e, props.userId, props.prospect.id)}>Connect</button></div> :
                         <div style={{paddingTop:15}} onClick={(e) => connectUnconnectUsers(e, props.userId, props.prospect.id)}><button>Unconnect</button></div>
                     }
+                    <br />
+                    <div style={{margin:"auto", height:180, width:150}}>
+                        {props.prospect.attributes.image ?
+                            <img src={prospectPhoto(props.prospect.attributes.image)} alt="user avatar" style={{width:"100%", height:"100%"}} /> :
+                            <img src={avatar} alt="anonymous avatar" style={{width:"100%", height:"100%"}} />
+                        }
+                    </div>
                     <p><label>Prospect ID: </label>{props.prospect.id}</p>
                     <p><label>Name: </label>{props.prospect.attributes.first_name} {props.prospect.attributes.last_name}</p>
                     <p><label>eMail: </label>{props.prospect.attributes.email}</p>
