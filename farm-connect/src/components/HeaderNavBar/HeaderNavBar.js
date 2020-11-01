92e6aeb63e999bd3d378f0ce5a67c4968fc2d155
Alvin Coulson <aaacoulson@gmail.com>
2020-11-01 06:51:47 -0500

    show user type on HeaderNavBar

--- farm-connect/src/components/HeaderNavBar/HeaderNavBar.js
+++ farm-connect/src/components/HeaderNavBar/HeaderNavBar.js
@@ -7,13 +7,18 @@ const HeadNavBar = (props) => {
         return `http://localhost:3000/${userPhoto}`;
     }
 
+    const userType = (type) => {
+        return type === "Farmer" ? "F" : "P";
+    }
+
     return (
         <div style={{marginBottom:"15px"}}>
             {props.userPhoto ?
                 <img src={userPhoto(props.userPhoto)} alt="user avatar" style={{height:25, width:25, borderRadius:"50%", marginRight:10}} /> :
                 <img src={avatar} alt="anonymous avatar" style={{height:25, width:25, borderRadius:"50%", marginRight:10}} />
             }
-            <span style={{marginRight: "15px"}}>{props.userAttributes.first_name} {props.userAttributes.last_name}</span>
+            <span style={{marginRight: "10px"}}>{props.userAttributes.first_name} {props.userAttributes.last_name}</span>
+            <span style={{padding:"1px 5px", border:"solid 2px", marginRight:"10px", borderRadius:"50%", color:"#3a5f0b", borderColor:"#3a5f0b"}}><strong>{userType(props.userAttributes.type)}</strong></span>
             <Link to="/signout" onClick={props.userSignOut}>Sign Out</Link>
         </div>
     )
