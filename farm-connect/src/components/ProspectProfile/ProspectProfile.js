777bca34a96350e5f0191c574c9bc02f77f681c1
Alvin Coulson <aaacoulson@gmail.com>
2020-10-31 23:45:39 -0400

    add Connect and Unconnect buttons to ProspectProfile

--- farm-connect/src/components/ProspectProfile/ProspectProfile.js
+++ farm-connect/src/components/ProspectProfile/ProspectProfile.js
@@ -2,12 +2,23 @@ import React from 'react';
 import Loader from '../Loader/Loader';
 
 const ProspectProfile = (props) => {
+    const consolidatedConnects = [ ...props.userAttributes.connects, ...props.userAttributes.inverse_connects ];
+    const isConnected = (prospectId) => {
+        return consolidatedConnects.find(connect => {
+            return connect.id === parseInt(prospectId);
+        });
+    }
+
     return (
         <div style={{width: "60%", display: "inline", float: "left"}}>
             { props.isLoadingProspect ?
                 <Loader /> :
                 <div>
                     <h3>Prospect</h3>
+                    {!isConnected(props.prospect.id) ?
+                        <div style={{paddingTop:15}}><button style={{backgroundColor:"#3a5f0b", color:"#FFFFFF"}}>Connect</button></div> :
+                        <div style={{paddingTop:15}}><button>Unconnect</button></div>
+                    }
                     <p><label>Prospect ID: </label>{props.prospect.id}</p>
                     <p><label>Name: </label>{props.prospect.attributes.first_name} {props.prospect.attributes.last_name}</p>
                     <p><label>eMail: </label>{props.prospect.attributes.email}</p>
