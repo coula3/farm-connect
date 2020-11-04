274d7ded509516fe4b84d5bbcd8284b4ceff2b0f
Alvin Coulson <aaacoulson@gmail.com>
2020-11-04 14:57:03 -0500

    refactor json render for messages on create action on UsersController

--- farm-connect-backend/app/controllers/api/v1/users_controller.rb
+++ farm-connect-backend/app/controllers/api/v1/users_controller.rb
@@ -18,7 +18,7 @@ class Api::V1::UsersController < ApplicationController
             @token = encode_token({user_id: @user.id})
             render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
         else
-            render json: { messages: @user.errors.full_messages.join(" ")}, status: :not_acceptable
+            render json: { messages: @user.errors.full_messages}, status: :not_acceptable
         end
     end
 
