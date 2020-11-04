93d28772a8a25665607d3e021efc95cf5cda76c2
Alvin Coulson <aaacoulson@gmail.com>
2020-11-04 16:07:34 -0500

    add 'editmode' to local state on EditUser and add method to switch Redux state to local state

--- farm-connect/src/containers/EditUser/EditUser.js
+++ farm-connect/src/containers/EditUser/EditUser.js
@@ -9,7 +9,21 @@ class EditUser extends Component {
             lastName: "",
             dateOfBirth: "",
             email: ""
-        }
+        },
+        editMode: false
+    }
+
+    handleSwitchState = () => {
+        this.setState({
+            editMode: true,
+            user: {
+                ...this.state.user,
+                firstName: this.props.userAttributes.first_name,
+                lastName: this.props.userAttributes.last_name,
+                dateOfBirth: this.props.userAttributes.date_of_birth.slice(0, 10),
+                email: this.props.userAttributes.email
+            }
+        })
     }
 
     handleChange = (e) => {
@@ -30,10 +44,10 @@ class EditUser extends Component {
         return (
             <div style={{width: "60%", display: "inline", float: "left"}}>
                 <form style={{padding: 10, marginBottom: "5px"}} onSubmit={this.handleSubmit}>
-                    <p><input type="text" name="firstName" value={this.state.user.firstName ? this.state.user.firstName : this.props.userAttributes.first_name} onChange={this.handleChange} /></p>
-                    <p><input type="text" name="lastName" value={this.state.user.lastName ? this.state.user.lastName : this.props.userAttributes.last_name} onChange={this.handleChange} /></p>
-                    <p><input type="date" name="dateOfBirth" value={this.state.user.dateOfBirth ? this.state.user.dateOfBirth : this.props.userAttributes.date_of_birth.slice(0, 10)}onChange={this.handleChange}  /></p>
-                    <p><input type="text" name="email" value={this.state.user.email ? this.state.user.email : this.props.userAttributes.email} onChange={this.handleChange} /></p>
+                    <p><input type="text" name="firstName" value={this.state.user.firstName ? this.state.user.firstName : this.props.userAttributes.first_name} onClick={this.handleSwitchState} onChange={this.handleChange} /></p>
+                    <p><input type="text" name="lastName" value={this.state.user.lastName ? this.state.user.lastName : this.props.userAttributes.last_name} onClick={this.handleSwitchState} onChange={this.handleChange} /></p>
+                    <p><input type="date" name="dateOfBirth" value={this.state.user.dateOfBirth ? this.state.user.dateOfBirth : this.props.userAttributes.date_of_birth.slice(0, 10)} onClick={this.handleSwitchState} onChange={this.handleChange}  /></p>
+                    <p><input type="text" name="email" value={this.state.user.email ? this.state.user.email : this.props.userAttributes.email} onClick={this.handleSwitchState} onChange={this.handleChange} /></p>
                     <br />
                     <button>Update</button>
                 </form>
