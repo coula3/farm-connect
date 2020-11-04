f08a3177d1c7d3f633c04746b0734d0a20248e02
Alvin Coulson <aaacoulson@gmail.com>
2020-11-04 15:10:58 -0500

    assign value for borderRadius dynamically on ErrorMessages based on number of msgs

--- farm-connect/src/components/ErrorMessages/ErrorMessages.js
+++ farm-connect/src/components/ErrorMessages/ErrorMessages.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 const ErrorMessages = (props) => {
     const errorList = (errorMessages) => {
+        let radiusValue;
+        errorMessages.length == 1 ? radiusValue = 15 : radiusValue = 0;
+
         return errorMessages.map((message, idx) => {
-            return <ul key={idx}>
-                <li style={{width:"60%", margin:"auto", backgroundColor:"#f2eeed", borderRadius:"15px", listStyle:"none", padding:5}}>{message}</li>
+            return <ul key={idx} style={{width:"60%", margin:"auto", backgroundColor:"#f2eeed", borderRadius:`${radiusValue}px`, listStyle:"none", padding:5}}>
+                <li style={{fontSize:"14px"}}>{message}</li>
             </ul>
         })
     }
