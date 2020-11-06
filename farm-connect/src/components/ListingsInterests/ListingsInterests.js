4046854d724430771d0321910930ea1c6990ca70
Alvin Coulson <aaacoulson@gmail.com>
2020-11-06 11:43:33 -0500

    add ListingsInterests component and import into ResourcesBoard

---
+++ farm-connect/src/components/ListingsInterests/ListingsInterests.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ListingsInterests = () => {
+    return (
+        <div>
+            ListingsInterests Component
+        </div>
+    )
+}
+
+export default ListingsInterests;
--- farm-connect/src/components/ResourcesBoard/ResourcesBoard.js
+++ farm-connect/src/components/ResourcesBoard/ResourcesBoard.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Prospects from '../Prospects/Prospects';
+import ListingsInterests from '../ListingsInterests/ListingsInterests';
 
 const ResourcesBoard = (props) => {
     return (
         <div style={{width: "15%", display: "inline", float: "left", border: "solid 1px"}}>
+            <ListingsInterests />
             <Prospects {...props} />
         </div>
     )
