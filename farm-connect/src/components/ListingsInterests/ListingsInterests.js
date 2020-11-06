967b5c9ec17c3cbebb5f6d0a070fcfb9fff07d85
Alvin Coulson <aaacoulson@gmail.com>
2020-11-06 14:08:22 -0500

    render listings with high interests on ListingsInterests

--- farm-connect/src/components/ListingsInterests/ListingsInterests.js
+++ farm-connect/src/components/ListingsInterests/ListingsInterests.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-const ListingsInterests = () => {
+const ListingsInterests = (props) => {
+    const listingsInterests = props.listingsInterests.map((listing, idx) => {
+        return (
+            <ul key={idx}>
+                <li>{listing[0]} - {listing[2]} ({listing[1]})</li>
+            </ul>
+        )
+    })
+
     return (
         <div>
-            ListingsInterests Component
+            {listingsInterests}
         </div>
     )
 }
