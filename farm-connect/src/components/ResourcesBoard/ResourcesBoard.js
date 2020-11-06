dff22778903b31413560fc097f2d41a6340a1be1
Alvin Coulson <aaacoulson@gmail.com>
2020-11-06 11:16:32 -0500

    move rendered prospects data from ResourcesBoard to own components 'Prospects'

--- farm-connect/src/components/ResourcesBoard/ResourcesBoard.js
+++ farm-connect/src/components/ResourcesBoard/ResourcesBoard.js
@@ -1,31 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import avatar from '../../assets/avatar.png';
+import Prospects from '../Prospects/Prospects';
 
 const ResourcesBoard = (props) => {
-    const prospectPhoto = (image) => {
-        return `http://localhost:3000/${image}`;
-    }
-
-    const image = (image) => {
-        if(image){
-            return <img src={prospectPhoto(image)} alt="prospect avatar" style={{height:25, width:25, borderRadius:"50%", marginRight:5}} />
-        } else {
-            return <img src={avatar} alt="anonymous avatar" style={{height:25, width:25, borderRadius:"50%", marginRight:5}} />
-        }
-    }
-
-    const prospects = props.prospects.map((prospect) => {
-        return (
-            <ul style={{listStyle:"none", paddingLeft:0}} key={prospect.id}>
-                <li>{image(prospect.attributes.image)} <Link to={`/prospects/${prospect.id}`} onClick={()=>props.fetchProspect(prospect.id)}>{prospect.attributes.first_name} {prospect.attributes.last_name}</Link></li>
-            </ul>
-        )
-    })
-
     return (
         <div style={{width: "15%", display: "inline", float: "left", border: "solid 1px"}}>
-            {prospects}
+            <Prospects {...props} />
         </div>
     )
 }
