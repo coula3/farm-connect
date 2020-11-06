9f58bea90debb844c2964a016ff1e2dbb11bd144
Alvin Coulson <aaacoulson@gmail.com>
2020-11-06 01:49:50 -0500

    add validations to Listing

--- farm-connect-backend/app/models/listing.rb
+++ farm-connect-backend/app/models/listing.rb
@@ -2,4 +2,8 @@ class Listing < ApplicationRecord
   belongs_to :user
   belongs_to :commodity
   has_many :interests
+
+  validates :availability, presence: true
+  validates :quantity, numericality: { greater_than: -1, allow_nil: true, message: "quantity must be a positive value" }
+  validates :information, length: { maximum: 255 }
 end
