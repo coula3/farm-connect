a827884593cb4f62f9f846313396d860a3cc954d
Alvin Coulson <aaacoulson@gmail.com>
2020-11-06 23:14:06 -0500

    refactor index action on ProspectsController to return max of 5 suggested prospects

--- farm-connect-backend/app/controllers/api/v1/prospects_controller.rb
+++ farm-connect-backend/app/controllers/api/v1/prospects_controller.rb
@@ -8,14 +8,27 @@ class Api::V1::ProspectsController < ApplicationController
     end
 
     def index
-        prospects = Prospect.where.not(id: params[:id]).shuffle
+        suggested_prospects_ids
+        suggested_prospects = suggested_prospects_ids.map {|id| Prospect.find(id)}
 
-        prospects_with_images = prospects.map do |prospect|
+        prospects_with_images = suggested_prospects.map do |prospect|
             @image = prospect.photo.attached? ? rails_blob_path(prospect.photo) : ""
             prospect.image = @image
             prospect
         end
 
         render json: ProspectSerializer.new(prospects_with_images)
+    end
+
+    private
+    def suggested_prospects_ids
+        prospects_ids = Prospect.where.not(id: params[:id]).map(&:id)
+        current_user = User.find_by(id: params[:id])
+
+        connects = current_user.connects.map(&:id)
+        inverse_connects = current_user.inverse_connects.map(&:id)
+        consolidated_connects = connects.concat(inverse_connects)
+
+        suggested_prospects = prospects_ids.reject {|prospect| consolidated_connects.include?(prospect)}.sample(5)
     end
 end
