04df560aa6ee625b64bb2ee5da18800ba3e6d32d
Alvin Coulson <aaacoulson@gmail.com>
2020-11-06 21:16:15 -0500

    pad listing ids on ListingsInterests

--- farm-connect/src/components/ListingsInterests/ListingsInterests.js
+++ farm-connect/src/components/ListingsInterests/ListingsInterests.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Loader from '../Loader/Loader';
+import { padIds } from '../../assets/Miscellaneous';
 
 const ListingsInterests = (props) => {
     const listingsInterests = props.listingsInterests.map((listing, idx) => {
         return (
             <ul style={{listStyle:"none", paddingLeft:"0px"}} key={idx}>
-                <li><Link to={`/listings/${listing[0]}`} onClick={() => props.fetchListing(listing[0])}>{listing[0]}</Link> - {listing[2]} ({listing[1]})</li>
+                <li><Link to={`/listings/${listing[0]}`} onClick={() => props.fetchListing(listing[0])}>{padIds(listing[0])}</Link> - {listing[2]} ({listing[1]})</li>
             </ul>
         )
     })
