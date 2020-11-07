5278bb2d41246920c41ec8d8fe70e8dfbe3fcc21
Alvin Coulson <aaacoulson@gmail.com>
2020-11-06 23:55:37 -0500

    remove commented code from InterestsController

--- farm-connect-backend/app/controllers/api/v1/interests_controller.rb
+++ farm-connect-backend/app/controllers/api/v1/interests_controller.rb
@@ -4,11 +4,7 @@ class Api::V1::InterestsController < ApplicationController
         listing_interests_array = listing_interests_hash.sort_by {|k, v| v}.reverse.slice(0..4)
         listings_commodity_names = listing_interests_array.map {|listing| Listing.find(listing[0]).commodity.name}
         listing_interests = listing_interests_array.each.with_index {|element, idx| element.push(listings_commodity_names[idx])}
-
-        # listings_interests_hash = {}
-        # listing_interests.each.with_index(1) do |element, idx|
-        #     listings_interests_hash[idx] = element
-        # end
+        # listing_interests = listing_interests_array.each.with_index {|element, idx| element.unshift(idx + 1).push(listings_commodity_names[idx])}
 
         render json: {interests: listing_interests}
     end
