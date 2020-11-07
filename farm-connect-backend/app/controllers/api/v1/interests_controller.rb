96a327e52b27c90c1f57b7afef347d7f59e56e87
Alvin Coulson <aaacoulson@gmail.com>
2020-11-07 11:14:58 -0500

    fix bug - return to FE only high interest open listings

--- farm-connect-backend/app/controllers/api/v1/interests_controller.rb
+++ farm-connect-backend/app/controllers/api/v1/interests_controller.rb
@@ -1,10 +1,13 @@
 class Api::V1::InterestsController < ApplicationController
     def index
-        listing_interests_hash = Interest.group(:listing_id).count
+        open_listing_with_interests =  Interest.select {|interest| !interest.listing.closed }.pluck(:listing_id)
+
+        listing_interests_hash = Hash.new(0)
+        open_listing_with_interests.each {|e| listing_interests_hash[e] += 1}
+
         listing_interests_array = listing_interests_hash.sort_by {|k, v| v}.reverse.slice(0..4)
         listings_commodity_names = listing_interests_array.map {|listing| Listing.find(listing[0]).commodity.name}
         listing_interests = listing_interests_array.each.with_index {|element, idx| element.push(listings_commodity_names[idx])}
-        # listing_interests = listing_interests_array.each.with_index {|element, idx| element.unshift(idx + 1).push(listings_commodity_names[idx])}
 
         render json: {interests: listing_interests}
     end
