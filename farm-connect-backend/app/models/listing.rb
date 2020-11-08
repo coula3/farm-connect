29d2e0c4cf9d6f1ffe3e891b88b0c9c81cc743c8
Alvin Coulson <aaacoulson@gmail.com>
2020-11-07 22:44:13 -0500

    add 'require_measure' validation to Listing

--- farm-connect-backend/app/models/listing.rb
+++ farm-connect-backend/app/models/listing.rb
@@ -7,10 +7,17 @@ class Listing < ApplicationRecord
   validates :quantity, numericality: { greater_than: -1, allow_nil: true, message: "quantity must be a positive value" }
   validates :information, length: { maximum: 255 }
   validate :availability_not_before_today
+  validate :require_measure
 
   def availability_not_before_today
     if availability.present? && availability < Date.today
         errors.add(:availability, "must not be before today")
+    end
+  end
+
+  def require_measure
+    if !measure.present? && quantity > -1
+      errors.add(:measure, "can't be blank")
     end
   end
 end
