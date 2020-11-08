eb10073fd3b1463e29bf10269bd896cc62f2aed3
Alvin Coulson <aaacoulson@gmail.com>
2020-11-07 19:33:50 -0500

    add custom validation 'availability_not_before_today' on Listing

--- farm-connect-backend/app/models/listing.rb
+++ farm-connect-backend/app/models/listing.rb
@@ -6,4 +6,11 @@ class Listing < ApplicationRecord
   validates :availability, presence: true
   validates :quantity, numericality: { greater_than: -1, allow_nil: true, message: "quantity must be a positive value" }
   validates :information, length: { maximum: 255 }
+  validate :availability_not_before_today
+
+  def availability_not_before_today
+    if availability.present? && availability < Time.now
+        errors.add(:availability, "must not be before today")
+    end
+  end
 end
