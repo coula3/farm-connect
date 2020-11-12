7bca097b34d1b80e5c9e928ce2e3397db87ab769
Alvin Coulson <aaacoulson@gmail.com>
2020-11-12 14:48:43 -0500

    remove redundant conditional rendering of 'Close' on SideNavBar. Ref: componentDidUpdate on Listings

--- farm-connect/src/components/SideNavBar/SideNavBar.js
+++ farm-connect/src/components/SideNavBar/SideNavBar.js
@@ -22,10 +22,7 @@ const SideNavBar = (props) => {
                     <br />
                     <span style={{color:"#3a5f0b"}}><strong>My Listings</strong></span>
                     <p><Link to={`/users/${props.userId}/listings`} onClick={handleClick}>Open</Link></p>
-                    { props.location.pathname === "/listings" || props.location.pathname === "/listings/other-farmers" || props.location.pathname === `/users/${props.userId}/closed-listings` || props.location.pathname === `/users/${props.userId}/listings` ?
-                        <p><Link to={`/users/${props.userId}/closed-listings`} onClick={() => {props.fetchUserClosedListings(props.userId); props.listingsUnrendered()}}>Closed</Link></p> :
-                        null
-                    }
+                    <p><Link to={`/users/${props.userId}/closed-listings`} onClick={() => {props.fetchUserClosedListings(props.userId); props.listingsUnrendered()}}>Closed</Link></p>
                 </>
             )
         }
