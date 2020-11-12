db30fa6a40d27f139b23f0ecf655c35bc806edea
Alvin Coulson <aaacoulson@gmail.com>
2020-11-12 16:10:37 -0500

    import sadEmoji into NoMatch

--- farm-connect/src/components/NoMatch/NoMatch.js
+++ farm-connect/src/components/NoMatch/NoMatch.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import sadEmoji from '../../assets/sadEmoji.png';
 
 const NoMatch = (props) => {
     return (
-        <h4>
-            404 - Pathname <span style={{color:"red"}}>"{props.location.pathname}"</span> not found
-        </h4>
+        <>
+            <h4>
+                404 - Pathname <span style={{color:"red"}}>"{props.location.pathname}"</span> not found
+            </h4>
+            <img src={sadEmoji} alt="sad face" style={{marginTop:"15px", height:"20px", width:"20px"}}></img>
+        </>
     )
 }
 
