eb018abc958b76dfc2fd472ead0c25df41c8bcb0
Alvin Coulson <aaacoulson@gmail.com>
2020-11-12 23:40:32 -0500

    build update action on 'PhotosController'

--- farm-connect-backend/app/controllers/api/v1/photos_controller.rb
+++ farm-connect-backend/app/controllers/api/v1/photos_controller.rb
@@ -1,2 +1,11 @@
 class Api::V1::PhotosController < ApplicationController
+    def update
+        user = User.find_by(id: params[:id])
+        if params[:file]
+            user.photo.attach(params[:file])
+            @photo = rails_blob_path(user.photo)
+            user.update(photo: @photo)
+            render json: user
+        end
+    end
 end
