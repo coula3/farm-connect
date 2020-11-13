11e7a65b8f3815a3193f5283cf445ccc2cbfc6cc
Alvin Coulson <aaacoulson@gmail.com>
2020-11-13 10:52:39 -0500

    fix bug on render of NoMatch

--- farm-connect/src/components/NoMatch/NoMatch.js
+++ farm-connect/src/components/NoMatch/NoMatch.js
@@ -4,12 +4,12 @@ import sadEmoji from '../../assets/sadEmoji.png';
 
 const NoMatch = (props) => {
     return (
-        <>
+        <div style={{width: "60%", display: "inline", float: "left"}}>
             <h4>
                 404 - Pathname <span style={{color:"red"}}>"{props.location.pathname}"</span> not found
             </h4>
             <img src={sadEmoji} alt="sad face" style={{marginTop:"15px", height:"20px", width:"20px"}}></img>
-        </>
+        </div>
     )
 }
 
