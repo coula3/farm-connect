389bdafbda86cdfe752e3999c43c80c60b8565e6
Alvin Coulson <aaacoulson@gmail.com>
2020-11-14 12:23:54 -0500

    set width and padding of AppHeader

--- farm-connect/src/components/AppHeader/AppHeader.js
+++ farm-connect/src/components/AppHeader/AppHeader.js
@@ -3,7 +3,7 @@ import logoSrc from '../../assets/farmConnectLogo.png'
 
 const AppHeader = () => {
     return(
-        <div style={{margin: "15px 0px 40px 0px"}}>
+        <div style={{width:"90%", margin: "1% 5% 1% 5%", padding:"1% 0% 1% 0%"}}>
             <img alt="Farm Connect Logo" src={logoSrc} style={{height: "5%", width: "5%"}}/>
             <div style={{color: "#3a5f0b", fontSize: 50, fontFamily: 'Piedra'}}>farmConnect</div>
             <p>changing lives through connects</p>
