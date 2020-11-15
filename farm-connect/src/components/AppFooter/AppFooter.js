55c7abd408da53b7c7ae8c4a7b0b9a8fbdf18e06
Alvin Coulson <aaacoulson@gmail.com>
2020-11-15 12:04:24 -0500

    create AppFooter.css and move styling from related .js file

--- farm-connect/src/components/AppFooter/AppFooter.js
+++ farm-connect/src/components/AppFooter/AppFooter.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import './AppFooter.css';
 
-const AppFooter = () => {
-    return (
-        <div style={{clear:"both", position:"fixed", width:"90%", margin:"0% 5% 0% 5%", height:"75px", bottom:"10px", backgroundColor:"#FFF", paddingTop:"10px", borderRadius:"0px 0px 20px 20px"}}>
-            <small>&copy; 2020 <span>farmConnect</span></small>
-        </div>
-    )
-}
+const AppFooter = () => (
+    <div className="AppFooter_main_div">
+        <small>&copy; 2020 <span>farmConnect</span></small>
+    </div>
+)
 
 export default AppFooter;
