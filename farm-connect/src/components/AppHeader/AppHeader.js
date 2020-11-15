14f0d174fa1e7186f41298587e2549742ec9d218
Alvin Coulson <aaacoulson@gmail.com>
2020-11-15 02:33:12 -0500

    create AppHeader.css and move styling from related .js file

--- farm-connect/src/components/AppHeader/AppHeader.js
+++ farm-connect/src/components/AppHeader/AppHeader.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logoSrc from '../../assets/farmConnectLogo.png'
+import './AppHeader.css';
 
 const AppHeader = () => {
     return(
-        <div style={{width:"90%", margin: "1% 5% 1% 5%", padding:"1% 0% 1% 0%", borderRadius:"15px", backgroundColor:"#FFF"}}>
-            <img alt="Farm Connect Logo" src={logoSrc} style={{height: "5%", width: "5%"}}/>
-            <div style={{color: "#3a5f0b", fontSize: 50, fontFamily: 'Piedra'}}>farmConnect</div>
+        <div className="AppHeader_main_div">
+            <img id="logo_img" alt="Farm Connect Logo" src={logoSrc} />
+            <div id="name_div">farmConnect</div>
             <span>changing lives through connects</span>
         </div>
     )
