0bbd0927af23d031f5e0d491bc5a54e8d744d223
Alvin Coulson <aaacoulson@gmail.com>
2020-11-15 01:11:55 -0500

    create NoMatch.css and move styling from related .js file

--- farm-connect/src/components/NoMatch/NoMatch.js
+++ farm-connect/src/components/NoMatch/NoMatch.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import sadEmoji from '../../assets/sadEmoji.png';
+import './NoMatch.css';
 
 const NoMatch = (props) => {
     let divStyle;
-    props.isAuthenticated ? divStyle = {width:"65%", display:"inline", float:"left"} : divStyle = {style:"90%"};
+    props.isAuthenticated ? divStyle = "NoMatch_auth_main_div" : divStyle = "NoMatch_no_auth_main_div";
 
     return (
-        <div style={divStyle}>
+        <div className={divStyle}>
             <h4>
-                404 - Pathname <span style={{color:"red"}}>"{props.location.pathname}"</span> not found
+                404 - Pathname <span id="span_color">"{props.location.pathname}"</span> not found
             </h4>
-            <img src={sadEmoji} alt="sad face" style={{marginTop:"15px", height:"20px", width:"20px"}}></img>
+            <img src={sadEmoji} alt="sad face" id="emoji_img"></img>
         </div>
     )
 }
