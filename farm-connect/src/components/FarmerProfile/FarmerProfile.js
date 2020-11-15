1de3a52b92dd6571ab5c5b41bba5daa72cf801fd
Alvin Coulson <aaacoulson@gmail.com>
2020-11-15 02:17:56 -0500

    import css file into FarmerProfile

--- farm-connect/src/components/FarmerProfile/FarmerProfile.js
+++ farm-connect/src/components/FarmerProfile/FarmerProfile.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Loader from '../Loader/Loader';
 import avatar from '../../assets/avatar.png';
 import { getDate } from '../../utils/miscellaneousUtils';
+import './FarmerProfile.css';
 
 const FarmerProfile = (props) => {
     const consolidatedConnects = [ ...props.userAttributes.connects, ...props.userAttributes.inverse_connects ];
