5faa635df3b04a11c0a062914eda9308d922f347
Alvin Coulson <aaacoulson@gmail.com>
2020-11-15 02:14:41 -0500

    create FarmerProfile.css and move styling from related .js file

--- farm-connect/src/components/FarmerProfile/FarmerProfile.js
+++ farm-connect/src/components/FarmerProfile/FarmerProfile.js
@@ -36,30 +36,30 @@ const FarmerProfile = (props) => {
     }
 
     return (
-        <div style={{width: "65%", display: "inline", float: "left"}}>
+        <div className="FarmerProfile_main_div">
             { props.isLoadingFarmer ?
                 <Loader /> :
                 <div>
                     <h3>Farmer Profile</h3>
-                    <div style={{margin:"auto", height:180, width:150}}>
+                    <div id="img_div">
                         {props.farmerPhoto ?
-                            <img src={farmerPhoto(props.farmerPhoto)} alt="user avatar" style={{width:"100%", height:"100%"}} /> :
-                            <img src={avatar} alt="anonymous avatar" style={{width:"100%", height:"100%"}} />
+                            <img src={farmerPhoto(props.farmerPhoto)} alt="user avatar" className="farmer_img" /> :
+                            <img src={avatar} alt="anonymous avatar" className="farmer_img" />
                         }
                     </div>
-                    <p style={{marginTop:"15px"}}><label>Farmer ID: </label>{props.farmer.id}</p>
+                    <p id="id_p"><label>Farmer ID: </label>{props.farmer.id}</p>
                     <p><label>Name: </label>{props.farmer.attributes.first_name} {props.farmer.attributes.last_name}</p>
                     <p><label>eMail: </label>{props.farmer.attributes.email}</p>
                     <br />
                     <p><label>Joined: </label>{getDate(props.farmer.attributes.created_at)}</p>
                     {!isCurrentUser(props.farmer.id) ?
                         !isConnected ?
-                            <div style={{paddingTop:15}}><button style={{backgroundColor:"#3a5f0b", color:"#FFF"}} onClick={(e) => connectUnconnectUsers(e, props.userId, props.farmer.id)}>Connect</button></div> :
-                            <div style={{paddingTop:15}}><button onClick={(e) => connectUnconnectUsers(e, props.userId, props.farmer.id)}>Unconnect</button></div>
+                            <div className="connect_btn_div"><button id="connect_btn" onClick={(e) => connectUnconnectUsers(e, props.userId, props.farmer.id)}>Connect</button></div> :
+                            <div className="connect_btn_div"><button onClick={(e) => connectUnconnectUsers(e, props.userId, props.farmer.id)}>Unconnect</button></div>
                     :
                         null
                     }
-                    <div style={{marginTop:10}}><button style={{backgroundColor:"#3a5f0b", color:"#FFF"}} onClick={handleViewListings}>{displayMyOrName(props.farmer.id)}</button></div>
+                    <div id="listing_btn_div"><button id="listing_btn" onClick={handleViewListings}>{displayMyOrName(props.farmer.id)}</button></div>
                 </div>
             }
         </div>
