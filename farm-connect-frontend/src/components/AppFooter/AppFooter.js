dd9947a6cabe11eeb98ea5e8dada42c97c264fbd
Alvin Coulson <aaacoulson@gmail.com>
2020-11-17 15:25:51 -0500

    style link to TOS on AppFooter

--- farm-connect-frontend/src/components/AppFooter/AppFooter.js
+++ farm-connect-frontend/src/components/AppFooter/AppFooter.js
@@ -4,8 +4,7 @@ import './AppFooter.css';
 
 const AppFooter = (props) => (
     <div className="AppFooter_main_div">
-        <span><Link to="/terms-of-service">Terms of Service</Link></span>
-        <br />
+        <p id="tos_p"><Link id="tos_link" to="/terms-of-service">Terms of Service</Link></p>
         <small>&copy; {new Date().getUTCFullYear()} <span id="app_name_span"><strong>farmConnect</strong></span></small>
     </div>
 )
