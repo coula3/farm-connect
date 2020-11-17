6a452d4cd785567f2d66483b4d5dbf224d35b1ab
Alvin Coulson <aaacoulson@gmail.com>
2020-11-17 15:14:39 -0500

    add 'Terms of Service' to AppFooter

--- farm-connect-frontend/src/components/AppFooter/AppFooter.js
+++ farm-connect-frontend/src/components/AppFooter/AppFooter.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
 import './AppFooter.css';
 
-const AppFooter = () => (
+const AppFooter = (props) => (
     <div className="AppFooter_main_div">
+        <span><Link to="/terms-of-service">Terms of Service</Link></span>
+        <br />
         <small>&copy; {new Date().getUTCFullYear()} <span id="app_name_span"><strong>farmConnect</strong></span></small>
     </div>
 )
 
-export default AppFooter;
+export default withRouter(AppFooter);
