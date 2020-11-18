9d4cbb2ef5c657a69684579673b9e5a1980ebb74
Alvin Coulson <aaacoulson@gmail.com>
2020-11-18 15:31:39 -0500

    refactor index action on InterestsController to include user id in json object

--- farm-connect-backend/app/controllers/api/v1/interests_controller.rb
+++ farm-connect-backend/app/controllers/api/v1/interests_controller.rb
@@ -1,13 +1,14 @@
 class Api::V1::InterestsController < ApplicationController
     def index
-        open_listing_with_interests =  Interest.select {|interest| !interest.listing.closed }.pluck(:listing_id)
+        open_listing_ids_with_interests =  Interest.select {|interest| !interest.listing.closed }.pluck(:listing_id)
 
         listing_interests_hash = Hash.new(0)
-        open_listing_with_interests.each {|e| listing_interests_hash[e] += 1}
+        open_listing_ids_with_interests.each {|e| listing_interests_hash[e] += 1}
 
         listing_interests_array = listing_interests_hash.sort_by {|k, v| v}.reverse.slice(0..4)
         listings_commodity_names = listing_interests_array.map {|listing| Listing.find(listing[0]).commodity.name}
-        listing_interests = listing_interests_array.each.with_index {|element, idx| element.push(listings_commodity_names[idx])}
+        listings_user_ids = listing_interests_array.map {|listing| Listing.find(listing[0]).user.id}
+        listing_interests = listing_interests_array.each.with_index {|element, idx| element.push(listings_commodity_names[idx]).push(listings_user_ids[idx])}
 
         render json: {interests: listing_interests}
     end
