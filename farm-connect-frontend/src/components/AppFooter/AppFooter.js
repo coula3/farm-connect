287069215d7e01f1de6e2d90b95d458767d02b01
Alvin Coulson <aaacoulson@gmail.com>
2020-11-18 11:25:59 -0500

    fix bug - prevent multiple clicks on 'Terms of Service' on AppFooter to add TOS component multiple times to the history stack

--- farm-connect-frontend/src/components/AppFooter/AppFooter.js
+++ farm-connect-frontend/src/components/AppFooter/AppFooter.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import './AppFooter.css';
 
-const AppFooter = (props) => (
-    <div className="AppFooter_main_div">
-        <p id="tos_p"><Link id="tos_link" to="/terms-of-service">Terms of Service</Link></p>
-        <small>&copy; {new Date().getUTCFullYear()} <span id="app_name_span"><strong>farmConnect</strong></span></small>
-    </div>
-)
+const AppFooter = (props) => {
+    const handleClick = () => {
+        props.location.pathname !== "/terms-of-service" && props.history.push("/terms-of-service");
+    }
+
+    return (
+        <div className="AppFooter_main_div">
+            <p id="tos_p" onClick={handleClick}>Terms of Service</p>
+            <small>&copy; {new Date().getUTCFullYear()} <span id="app_name_span"><strong>farmConnect</strong></span></small>
+        </div>
+    )
+}
 
 export default withRouter(AppFooter);
