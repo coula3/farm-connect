a87e3b572f17b53ea38c941d27ecf4e735d27f91
Alvin Coulson <aaacoulson@gmail.com>
2020-11-25 14:57:44 -0500

    add events to search input to highlight/unhighlight the column headers being searched

--- farm-connect-frontend/src/containers/Listings/Listings.js
+++ farm-connect-frontend/src/containers/Listings/Listings.js
@@ -14,6 +14,7 @@ import './Listings.css';
 class Listings extends React.Component {
     state = {
         searchText: "",
+        searchInputFocused: false,
         isAvailableSorted: false
     }
 
@@ -59,6 +60,18 @@ class Listings extends React.Component {
         });
     }
 
+    handleFocus = () => {
+        this.setState({
+            searchInputFocused: true
+        });
+    }
+
+    handleBlur = () => {
+        this.setState({
+            searchInputFocused: false
+        });
+    }
+
     handleSortAvailable = () => {
         this.setState({
             isAvailableSorted: !this.state.isAvailableSorted
@@ -66,7 +79,7 @@ class Listings extends React.Component {
     }
 
     getTableHeadSearchStyles = () => {
-        return this.state.searchText ? "search_styles" : null;
+        return this.state.searchInputFocused ? "search_styles" : null;
     }
 
     getAvailableStyles = () => {
@@ -223,7 +236,7 @@ class Listings extends React.Component {
                                     :   <h4>No Closed Listing</h4>
                             }
 
-                            <input id="search_input" type="text" placeholder="enter search text" value={this.state.searchText} onChange={this.handleChange} />
+                            <input id="search_input" type="text" placeholder="enter search text" value={this.state.searchText} onFocus={this.handleFocus} onBlur={this.handleBlur} onChange={this.handleChange} />
 
                             {this.props.listings[0] && renderListingsTable()}
                         </>
