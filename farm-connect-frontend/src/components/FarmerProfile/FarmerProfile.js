0e9e8c0df8189d55d47ea4349c2f3c75ba174490
Alvin Coulson <aaacoulson@gmail.com>
2020-11-24 23:26:57 -0500

    refactor conditional statements on FarmerProfile

--- farm-connect-frontend/src/components/FarmerProfile/FarmerProfile.js
+++ farm-connect-frontend/src/components/FarmerProfile/FarmerProfile.js
@@ -38,33 +38,33 @@ const FarmerProfile = (props) => {
 
     return (
         <div className="FarmerProfile_main_div">
-            { props.isLoadingFarmer ?
-                <Loader /> :
-                <div className="farmer_profile_card">
-                    <h3>Farmer Profile</h3>
+            { props.isLoadingFarmer
+                ?   <Loader />
+                :   <div className="farmer_profile_card">
+                        <h3>Farmer Profile</h3>
 
-                    <div id="img_div">
-                        {props.farmerPhoto ?
-                            <img src={farmerPhoto(props.farmerPhoto)} alt="user avatar" className="farmer_img" /> :
-                            <img src={avatar} alt="anonymous avatar" className="farmer_img" />
+                        <div id="img_div">
+                            { props.farmerPhoto
+                                ?   <img src={farmerPhoto(props.farmerPhoto)} alt="user avatar" className="farmer_img" />
+                                :   <img src={avatar} alt="anonymous avatar" className="farmer_img" />
+                            }
+                        </div>
+
+                        <p id="id_p"><label><strong>ID:</strong> </label>{props.farmer.id}</p>
+                        <p><label><strong>Name:</strong> </label>{props.farmer.attributes.first_name} {props.farmer.attributes.last_name}</p>
+                        <p><label><strong>Email:</strong> </label>{props.farmer.attributes.email}</p>
+                        <br />
+                        <p><label><strong>Joined:</strong> </label>{getDate(props.farmer.attributes.created_at)}</p>
+
+                        { !isCurrentUser(props.farmer.id)
+                            ?   !isConnected
+                                ?   <div className="connect_btn_div"><button id="connect_btn" className="global_btn" onClick={(e) => connectUnconnectUsers(e, props.userId, props.farmer.id)}>Connect</button></div>
+                                :   <div className="connect_btn_div"><button id="unconnect_btn" onClick={(e) => connectUnconnectUsers(e, props.userId, props.farmer.id)}>Unconnect</button></div>
+
+                            :   null
                         }
+                        <div id="listing_btn_div"><button id="listing_btn" className="global_btn" onClick={handleViewListings}>{displayMyOrName(props.farmer.id)}</button></div>
                     </div>
-
-                    <p id="id_p"><label><strong>ID:</strong> </label>{props.farmer.id}</p>
-                    <p><label><strong>Name:</strong> </label>{props.farmer.attributes.first_name} {props.farmer.attributes.last_name}</p>
-                    <p><label><strong>Email:</strong> </label>{props.farmer.attributes.email}</p>
-                    <br />
-                    <p><label><strong>Joined:</strong> </label>{getDate(props.farmer.attributes.created_at)}</p>
-
-                    {!isCurrentUser(props.farmer.id) ?
-                        !isConnected ?
-                            <div className="connect_btn_div"><button id="connect_btn" className="global_btn" onClick={(e) => connectUnconnectUsers(e, props.userId, props.farmer.id)}>Connect</button></div> :
-                            <div className="connect_btn_div"><button id="unconnect_btn" onClick={(e) => connectUnconnectUsers(e, props.userId, props.farmer.id)}>Unconnect</button></div>
-                    :
-                        null
-                    }
-                    <div id="listing_btn_div"><button id="listing_btn" className="global_btn" onClick={handleViewListings}>{displayMyOrName(props.farmer.id)}</button></div>
-                </div>
             }
         </div>
     )
