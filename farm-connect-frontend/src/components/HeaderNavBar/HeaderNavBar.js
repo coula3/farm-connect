c4606d6e17b503c0eccf714da92379e08ebf2c30
Alvin Coulson <aaacoulson@gmail.com>
2020-11-24 23:29:18 -0500

    refactor conditional statements on HeaderNavBar

--- farm-connect-frontend/src/components/HeaderNavBar/HeaderNavBar.js
+++ farm-connect-frontend/src/components/HeaderNavBar/HeaderNavBar.js
@@ -14,9 +14,9 @@ const HeadNavBar = (props) => {
 
     return (
         <div className="HeaderNavBar_main_div">
-            {props.userPhoto ?
-                <img className="header_img" src={userPhoto(props.userPhoto)} alt="user avatar" /> :
-                <img className="header_img" src={avatar} alt="anonymous avatar" />
+            { props.userPhoto
+                ?   <img className="header_img" src={userPhoto(props.userPhoto)} alt="user avatar" />
+                :   <img className="header_img" src={avatar} alt="anonymous avatar" />
             }
             <Link id="hnb_name_link" to={`/users/${props.userId}`} title="View Profile">{props.userAttributes.first_name} {props.userAttributes.last_name}</Link>
             <span id="user_type_span"><strong>{userType(props.userAttributes.type)}</strong></span>
