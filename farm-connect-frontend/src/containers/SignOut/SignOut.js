f6b5d6d359b8e212c348926910946492cfda4133
Alvin Coulson <aaacoulson@gmail.com>
2020-12-08 11:32:03 -0500

    redirect to '/' on sign out

--- farm-connect-frontend/src/containers/SignOut/SignOut.js
+++ farm-connect-frontend/src/containers/SignOut/SignOut.js
@@ -1,22 +1,15 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { signOutUser } from '../../actions/userActions';
 
 class SignOut extends React.Component {
     componentDidMount(){
-        return this.props.isAuthenticated && this.props.signOutUser();
+        this.props.isAuthenticated && this.props.signOutUser();
     }
 
     render(){
-        return (
-            <div style={{marginTop:"25px"}}>
-                You have successfully signed out!
-                <div style={{marginTop:"40px"}}>
-                    <Link to="/">Home</Link>
-                </div>
-            </div>
-        );
+        return <Redirect to="/" />;
     }
 }
 
