afe543d318b08a5cd0db65d4390b35560cb20bd0
Alvin Coulson <aaacoulson@gmail.com>
2020-12-11 19:28:00 -0500

    build index action on ConnectionsController to return user connects to FE

--- farm-connect-backend/app/controllers/api/v1/connections_controller.rb
+++ farm-connect-backend/app/controllers/api/v1/connections_controller.rb
@@ -1,2 +1,9 @@
 class Api::V1::ConnectionsController < ApplicationController
+    def index
+        user = User.find_by(id: params[:id])
+        consolidated_connections = user.connections << user.inverse_connections
+        user_connections = consolidated_connections.map { |connect| [connect, User.find(connect.connect_id).type] }
+
+        render json: { data: user_connections }
+    end
 end
