abc2bb8304124ce6489ed0be3b602edbe738481c
Alvin Coulson <aaacoulson@gmail.com>
2020-12-13 13:50:54 -0500

    add event listiner to fire callback function to render farmer/prospect profile

--- farm-connect-frontend/src/components/ConnectRequests/ConnectRequests.js
+++ farm-connect-frontend/src/components/ConnectRequests/ConnectRequests.js
@@ -4,6 +4,14 @@ import './ConnectRequests.css';
 
 const ConnectRequests = (props) => {
     const totalConnectRequests = props.userConnects.filter(connect => connect[0].status === "pending" && connect[0].user_id !== parseInt(props.userId));
+
+    const fetchProfile = (connectId, connectType) => {
+        if(connectType === "Farmer"){
+            props.fetchFarmer(connectId);
+        } else {
+            props.fetchProspect(connectId);
+        }
+    }
     
     return (
         <div className="ConnectRequests_main_div">
@@ -11,7 +19,7 @@ const ConnectRequests = (props) => {
                 <h3>Connect Requests</h3>
                 <ul id="connect_requests_ul">
                     {totalConnectRequests.map(connect => 
-                        <li id="connect_li" key={connect[0].id}><Link to={`/${connect[3].toLowerCase() + "s"}/${connect[0].user_id}`}>{connect[1]} {connect[2]}</Link></li>)
+                        <li id="connect_li" key={connect[0].id}><Link to={`/${connect[3].toLowerCase() + "s"}/${connect[0].user_id}`} onClick={() => fetchProfile(connect[0].user_id, connect[3])}>{connect[1]} {connect[2]}</Link></li>)
                 }
                 </ul>
             </div>
