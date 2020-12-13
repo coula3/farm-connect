de703d37415544047e55683725931da996e9ce21
Alvin Coulson <aaacoulson@gmail.com>
2020-12-13 14:13:58 -0500

    add user type to list on ConnectRequests

--- farm-connect-frontend/src/components/ConnectRequests/ConnectRequests.js
+++ farm-connect-frontend/src/components/ConnectRequests/ConnectRequests.js
@@ -19,7 +19,7 @@ const ConnectRequests = (props) => {
                 <h3>Connect Requests</h3>
                 <ul id="connect_requests_ul">
                     {totalConnectRequests.map(connect => 
-                        <li id="connect_li" key={connect[0].id}><Link to={`/${connect[3].toLowerCase() + "s"}/${connect[0].user_id}`} onClick={() => fetchProfile(connect[0].user_id, connect[3])}>{connect[1]} {connect[2]}</Link></li>)
+                        <li id="connect_li" key={connect[0].id}><Link to={`/${connect[3].toLowerCase() + "s"}/${connect[0].user_id}`} onClick={() => fetchProfile(connect[0].user_id, connect[3])}>{connect[1]} {connect[2]}</Link><span id="user_list_type_span">{connect[3][0]}</span></li>)
                 }
                 </ul>
             </div>
