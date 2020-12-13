b525d51a934c46f8c7dc8f3cc2ebe69ea8a07773
Alvin Coulson <aaacoulson@gmail.com>
2020-12-12 22:58:09 -0500

    refactor FarmerProfile to process 'Request Connect' and 'Accept' and make changes to related css files

--- farm-connect-frontend/src/components/FarmerProfile/FarmerProfile.js
+++ farm-connect-frontend/src/components/FarmerProfile/FarmerProfile.js
@@ -5,9 +5,25 @@ import { getDate, getFullName } from '../../utils/miscellaneousUtils';
 import './FarmerProfile.css';
 
 const FarmerProfile = (props) => {
-    const isConnected = props.userConnects.find(connect => {
-        return connect[0].connect_id === parseInt(props.farmer.id) && connect[0].status;
+    let usersConnected, pendingAcceptance;
+
+    const connectionByUser = !props.userConnects ? false : props.userConnects.find(connect => {
+        return connect[0].user_id === parseInt(props.userId) && connect[0].connect_id === parseInt(props.farmer.id);
     });
+
+    if(!props.userConnects){
+        usersConnected = false;
+    } else {
+        props.userConnects.find(connect => {
+            return usersConnected = (connect[0].connect_id === parseInt(props.farmer.id) || connect[0].user_id === parseInt(props.farmer.id)) && connect[0].status;
+        });
+    }
+
+    if(props.userConnects){
+        props.userConnects.find(connect => {
+            return pendingAcceptance = (connect[0].connect_id === parseInt(props.farmer.id) || connect[0].user_id === parseInt(props.farmer.id)) && connect[0].status === "pending";
+        });
+    }
 
     const farmerPhoto = (farmerPhoto) => {
        return `http://localhost:3000/${farmerPhoto}`;
@@ -24,8 +40,11 @@ const FarmerProfile = (props) => {
     }
 
     const connectUnconnectUsers = (e, userId, farmerId) => {
-        if(e.target.innerText === "Connect"){
+        if(e.target.innerText === "Request Connect"){
             props.requestConnect(userId, farmerId);
+        } else if(e.target.innerText === "Accept"){
+            props.acceptConnect(userId, farmerId);
+            console.log("Accept")
         } else {
             props.unConnectUsers(userId, farmerId);
         }
@@ -53,16 +72,23 @@ const FarmerProfile = (props) => {
                         <p><label><strong>Name:</strong> </label>{getFullName(props.farmer.attributes.first_name, props.farmer.attributes.last_name)}</p>
                         <p><label><strong>Email:</strong> </label>{props.farmer.attributes.email.toLowerCase()}</p>
                         <br />
-                        <p><label><strong>Joined:</strong> </label>{getDate(props.farmer.attributes.created_at)}</p>
+                        <p style={{marginBottom:"50px"}}><label><strong>Joined:</strong> </label>{getDate(props.farmer.attributes.created_at)}</p>
 
                         { !isCurrentUser(props.farmer.id)
-                            ?   !isConnected
-                                ?   <div className="connect_btn_div"><button className="global_btn connect_btn" onClick={(e) => connectUnconnectUsers(e, props.userId, props.farmer.id)}>Connect</button></div>
-                                :   <div className="connect_btn_div"><button className="unconnect_btn" onClick={(e) => connectUnconnectUsers(e, props.userId, props.farmer.id)}>Unconnect</button></div>
+                            ?   !usersConnected
+                                ?   <button className="global_btn request_btn" onClick={(e) => connectUnconnectUsers(e, props.userId, props.farmer.id)}>Request Connect</button>
+                                    :   pendingAcceptance && !connectionByUser
+                                        ?   <>
+                                                <button className="global_btn accept_btn" onClick={(e) => connectUnconnectUsers(e, props.userId, props.farmer.id)}>Accept</button>
+                                                <button className="global_btn decline_btn" onClick={(e) => connectUnconnectUsers(e, props.userId, props.farmer.id)}>Decline</button>
+                                            </>
+                                            : pendingAcceptance && connectionByUser
+                                                ?   <button className="global_btn cancel_request_btn" onClick={(e) => connectUnconnectUsers(e, props.userId, props.farmer.id)}>Cancel Request</button>
 
+                                                :   <div className="connect_btn_div"><button className="unconnect_btn" onClick={(e) => connectUnconnectUsers(e, props.userId, props.farmer.id)}>Unconnect</button></div>
                             :   null
                         }
-                        <div id="listing_btn_div"><button id="listing_btn" className="global_btn" onClick={handleViewListings}>{displayMyOrName(props.farmer.id)}</button></div>
+                        <div id="listing_btn_div"><button id="listings_btn" className="global_btn" onClick={handleViewListings}>{displayMyOrName(props.farmer.id)}</button></div>
                     </div>
             }
         </div>
