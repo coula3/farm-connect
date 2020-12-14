3de24c132672d81e8c9eb8bfd5f03e55790f1b61
Alvin Coulson <aaacoulson@gmail.com>
2020-12-13 19:36:08 -0500

    refactor the render of lists on Connects

--- farm-connect-frontend/src/components/Connects/Connects.js
+++ farm-connect-frontend/src/components/Connects/Connects.js
@@ -16,14 +16,18 @@ const Connects = (props) => {
         }
     }
     
+    const renderRequestsList = (connect) => {
+        return <li id="connect_li" key={connect[0].id}><Link to={`/${connect[3].toLowerCase() + "s"}/${connect[0].user_id}`} onClick={() => fetchProfile(connect[0].user_id, connect[3])}>{connect[1]} {connect[2]}</Link><span id="user_list_type_span">{connect[3][0]}</span></li>;
+    }
+
     return (
         <div className="ConnectRequests_main_div">
             <div className="connect_requests_card">
                 <h3>{headerText}</h3>
                 <ul id="connect_requests_ul">
                     {connects.map(connect => 
-                        <li id="connect_li" key={connect[0].id}><Link to={`/${connect[3].toLowerCase() + "s"}/${connect[0].user_id}`} onClick={() => fetchProfile(connect[0].user_id, connect[3])}>{connect[1]} {connect[2]}</Link><span id="user_list_type_span">{connect[3][0]}</span></li>)
-                }
+                        renderRequestsList(connect))
+                    }
                 </ul>
             </div>
         </div>
