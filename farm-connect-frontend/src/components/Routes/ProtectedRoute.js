e01f01d2e5e80224bec884f222f517461ae7b0a9
Alvin Coulson <aaacoulson@gmail.com>
2021-01-24 18:12:05 -0500

    refactor ProtectedRoute

--- farm-connect-frontend/src/components/Routes/ProtectedRoute.js
+++ farm-connect-frontend/src/components/Routes/ProtectedRoute.js
@@ -8,62 +8,14 @@ import { fetchFarmer } from '../../actions/farmersActions';
 
 const ProtectedRoute = ({
         isAuthenticated,
-        userId,
-        userAttributes,
-        hasListingChanged,
-        openListingsRendered,
-        removeUserListingInterest,
-        addUserListingInterest,
-        fetchFarmer,
-        fetchListing,
-        fetchListings,
-        commodities,
-        listing,
-        isLoading,
-        isLoadingProspect,
-        prospect,
-        prospects,
-        isLoadingFarmer,
-        farmer,
-        userConnects,
-        farmerPhoto,
-        userPhoto,
-        fetchProspect,
-        requestConnect,
-        acceptConnect,
-        unConnectUsers,
         component: Component,
         ...rest
     }) => {
+
     return (
-        <Route {...rest} render={(props) => {
+        <Route {...rest} render={(routerProps) => {
             return isAuthenticated
-                ?   <Component {...props}
-                        commodities={commodities}
-                        userId={userId}
-                        userAttributes={userAttributes}
-                        hasListingChanged={hasListingChanged}
-                        openListingsRendered={openListingsRendered}
-                        fetchFarmer={fetchFarmer}
-                        fetchListing={fetchListing}
-                        fetchListings={fetchListings}
-                        listing={listing}
-                        isLoading={isLoading}
-                        isLoadingProspect={isLoadingProspect}
-                        prospect={prospect}
-                        prospects={prospects}
-                        isLoadingFarmer={isLoadingFarmer}
-                        farmer={farmer}
-                        userConnects={userConnects}
-                        userPhoto={userPhoto}
-                        farmerPhoto={farmerPhoto}
-                        fetchProspect={fetchProspect}
-                        requestConnect={requestConnect}
-                        acceptConnect={acceptConnect}
-                        unConnectUsers={unConnectUsers}
-                        removeUserListingInterest={removeUserListingInterest}
-                        addUserListingInterest={addUserListingInterest}
-                    />
+                ?   <Component {...routerProps} {...rest} />
                 :   <Redirect to="/" />
         }} />
     )
@@ -74,33 +26,32 @@ const mapStateToProps = (state) => {
         isAuthenticated: state.currentUser.isAuthenticated,
         userId: state.currentUser.userId,
         userAttributes: state.currentUser.userAttributes,
+        userPhoto: state.currentUser.photo,
+        userConnects: state.connects.userConnects,
         commodities: state.commodities.commodities,
-        isLoading: state.listings.isLoading,
         listing: state.listings.listing,
-        hasListingChanged: state.listings.hasListingChanged,
-        openListingsRendered: state.listings.openListingsRendered,
+        isLoading: state.listings.isLoading,
         isLoadingFarmer: state.farmers.isLoadingFarmer,
         farmer: state.farmers.farmer,
+        farmerPhoto: state.farmers.photo,
         isLoadingProspect: state.prospects.isLoadingProspect,
         prospect: state.prospects.prospect,
-        prospects: state.prospects.prospects,
-        farmerPhoto: state.farmers.photo,
-        userPhoto: state.currentUser.photo,
-        userConnects: state.connects.userConnects
+        hasListingChanged: state.listings.hasListingChanged,
+        openListingsRendered: state.listings.openListingsRendered
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
     return {
+        fetchFarmer: (farmerId) => dispatch(fetchFarmer(farmerId)),
+        fetchProspect: (prospectId) => dispatch(fetchProspect(prospectId)),
+        fetchListing: (id) => dispatch(listingsActions.fetchListing(id)),
+        fetchListings: (farmer, routerProps) => dispatch(listingsActions.fetchListings(farmer, routerProps)),
         removeUserListingInterest: (listingId, interestId, currentUserId) => dispatch(listingsActions.removeUserListingInterest(listingId, interestId, currentUserId)),
         addUserListingInterest: (currentUserId, listingId) => dispatch(listingsActions.addUserListingInterest(currentUserId, listingId)),
         requestConnect: (currentUserId, connectId) => dispatch(requestConnect(currentUserId, connectId)),
         acceptConnect: (currentUserId, connectId) => dispatch(acceptConnect(currentUserId, connectId)),
         unConnectUsers: (currentUserId, connectId) => dispatch(unConnectUsers(currentUserId, connectId)),
-        fetchFarmer: (farmerId) => dispatch(fetchFarmer(farmerId)),
-        fetchProspect: (prospectId) => dispatch(fetchProspect(prospectId)),
-        fetchListing: (id) => dispatch(listingsActions.fetchListing(id)),
-        fetchListings: (farmer, routerProps) => dispatch(listingsActions.fetchListings(farmer, routerProps)),
     };
 }
 
