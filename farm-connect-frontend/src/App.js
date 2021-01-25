5edf8b33ed5e93f68aa8c7b1fead5adfbe06c577
Alvin Coulson <aaacoulson@gmail.com>
2021-01-25 14:57:03 -0500

    remove redundant props and methods from App.js as a result of the creation of ProtectedRoute

--- farm-connect-frontend/src/App.js
+++ farm-connect-frontend/src/App.js
@@ -7,10 +7,8 @@ import HeadNavBar from './components/HeaderNavBar/HeaderNavBar';
 import SideNavBar from './components/SideNavBar/SideNavBar';
 import Routes from './components/Routes/Routes';
 import { signOutUser } from './actions/userActions';
-import { requestConnect, acceptConnect, unConnectUsers } from './actions/connectionsActions';
 import * as listingsActions from './actions/listingsActions';
 import { fetchProspect } from './actions/prospectsActions';
-import { fetchFarmer } from './actions/farmersActions';
 import ResourcesBoard from './components/ResourcesBoard/ResourcesBoard';
 import { fetchListingsInterests } from './actions/interestsActions';
 import AppFooter from './components/AppFooter/AppFooter';
@@ -20,25 +18,8 @@ class App extends Component {
     this.props.signOutUser();
   }
 
-  handleRemoveUserListingInterest = (listingId, interestId, currentUserId) => {
-    const payload = {listing: {interestId: interestId, currentUserId: currentUserId}};
-    this.props.removeUserListingInterest(listingId, payload);
-  }
-
-  handleAddUserListingInterest = (currentUserId, listingId) => {
-    this.props.addUserListingInterest(currentUserId, listingId);
-  }
-
   fetchProspect = (id) => {
     this.props.fetchProspect(id);
-  }
-
-  handleRequestConnect = (currentUserId, connectId) => {
-    this.props.requestConnect(currentUserId, connectId);
-  }
-
-  handleUnconnectUsers = (currentUserId, connectId) => {
-    this.props.unConnectUsers(currentUserId, connectId)
   }
 
   render(){
@@ -78,42 +59,17 @@ class App extends Component {
               : null
             }
 
-            <Routes
-              isAuthenticated={this.props.isAuthenticated}
-              userId={this.props.userId}
-              userAttributes={this.props.userAttributes}
-              commodities={this.props.commodities}
-              isLoading={this.props.isLoading}
-              listing={this.props.listing}
-              hasListingChanged={this.props.hasListingChanged}
-              openListingsRendered={this.props.openListingsRendered}
-              isLoadingFarmer={this.props.isLoadingFarmer}
-              farmer={this.props.farmer}
-              isLoadingProspect={this.props.isLoadingProspect}
-              prospect={this.props.prospect}
-              farmerPhoto={this.props.farmerPhoto}
-              userPhoto={this.props.userPhoto}
-              userConnects={this.props.userConnects}
-              removeUserListingInterest={(listingId, interestId, currentUserId) => this.handleRemoveUserListingInterest(listingId, interestId, currentUserId)}
-              addUserListingInterest={(currentUserId, listingId) => this.handleAddUserListingInterest(currentUserId, listingId)}
-              requestConnect={(currentUserId, connectId) => this.handleRequestConnect(currentUserId, connectId)}
-              acceptConnect={(currentUserId, connectId) => this.props.acceptConnect(currentUserId, connectId)}
-              unConnectUsers={(currentUserId, farmerId) => this.handleUnconnectUsers(currentUserId, farmerId)}
-              fetchFarmer={(farmerId) => this.props.fetchFarmer(farmerId)}
-              fetchProspect={(prospectId) => this.props.fetchProspect(prospectId)}
-              fetchListing={(id) => this.props.fetchListing(id)}
-              fetchListings={(farmerId, routerProps) => this.props.fetchListings(farmerId, routerProps)}
-            />
+            <Routes />
 
             { this.props.isAuthenticated
               ? <ResourcesBoard
                   isLoadingProspects={this.props.isLoadingProspects}
                   userId={this.props.userId}
                   prospects={this.props.prospects}
-                  fetchProspect={(id) => this.fetchProspect(id)}
                   listingsInterests={this.props.listingsInterests}
-                  fetchListing={(id) => this.props.fetchListing(id)}
                   isLoadingInterests={this.props.isLoadingInterests}
+                  fetchProspect={(id) => this.fetchProspect(id)}
+                  fetchListing={(id) => this.props.fetchListing(id)}
                 />
               : null
             }
@@ -131,20 +87,11 @@ const mapStateToProps = (state) => {
     userId: state.currentUser.userId,
     userAttributes: state.currentUser.userAttributes,
     isAuthenticated: state.currentUser.isAuthenticated,
-    commodities: state.commodities.commodities,
-    isLoading: state.listings.isLoading,
-    listing: state.listings.listing,
     countUserInterestsListings: state.listings.countUserInterestsListings,
     hasListingChanged: state.listings.hasListingChanged,
-    isLoadingFarmer: state.farmers.isLoadingFarmer,
-    farmer: state.farmers.farmer,
     isLoadingProspects: state.prospects.isLoadingProspects,
     prospects: state.prospects.prospects,
-    isLoadingProspect: state.prospects.isLoadingProspect,
-    prospect: state.prospects.prospect,
-    farmerPhoto: state.farmers.photo,
     userPhoto: state.currentUser.photo,
-    isLoadingUser: state.currentUser.isLoading,
     listingsInterests: state.interests.listingsInterests,
     isLoadingInterests: state.interests.isLoadingInterests,
     openListingsRendered: state.listings.openListingsRendered,
@@ -156,17 +103,11 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     signOutUser: () => dispatch(signOutUser()),
-    removeUserListingInterest: (listingId, payload) => dispatch(listingsActions.removeUserListingInterest(listingId, payload)),
-    addUserListingInterest: (currentUserId, listingId) => dispatch(listingsActions.addUserListingInterest(currentUserId, listingId)),
-    requestConnect: (currentUserId, connectId) => dispatch(requestConnect(currentUserId, connectId)),
-    acceptConnect: (currentUserId, connectId) => dispatch(acceptConnect(currentUserId, connectId)),
-    unConnectUsers: (currentUserId, connectId) => dispatch(unConnectUsers(currentUserId, connectId)),
     fetchListing: (id) => dispatch(listingsActions.fetchListing(id)),
     fetchUserClosedListings: (props) => dispatch(listingsActions.fetchUserClosedListings(props)),
     listingsRendered: () => dispatch(listingsActions.listingsRendered()),
     listingsUnrendered: () => dispatch(listingsActions.listingsUnrendered()),
     fetchListings: (farmer, routerProps) => dispatch(listingsActions.fetchListings(farmer, routerProps)),
-    fetchFarmer: (farmerId) => dispatch(fetchFarmer(farmerId)),
     fetchProspect: (prospectId) => dispatch(fetchProspect(prospectId)),
     fetchListingsInterests: () => dispatch(fetchListingsInterests()),
     fetchUserInterestsListings: (id) => dispatch(listingsActions.fetchUserInterestsListings(id))
