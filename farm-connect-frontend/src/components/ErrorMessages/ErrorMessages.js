9dfb9a5a862cd289254745af0679d26b760849e0
Alvin Coulson <aaacoulson@gmail.com>
2021-01-26 22:48:02 -0500

    fix bug on the direct render from URL entry of ErrorMessages

--- farm-connect-frontend/src/components/ErrorMessages/ErrorMessages.js
+++ farm-connect-frontend/src/components/ErrorMessages/ErrorMessages.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './ErrorMessages.css';
 import history from '../../utils/history';
 
 const ErrorMessages = (props) => {
+    useEffect(() => {
+        !props.errMessage && props.history.replace("/listings");
+    }, [props])
+
     return (
         <div className="ErrorMessage_main_div">
-            {console.log(props)}
             <div className="error_message_card">
                 <input id="err_msg_close_btn" type="submit" value="X" onClick={() => history.goBack()}/>
-                <h5 style={{clear:"both", marginTop:"40px"}}>{props.errMessage}</h5>
+                <h5 id="err_h5">{props.errMessage}</h5>
             </div>
         </div>
     )
