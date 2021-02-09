490b0477a166fc682b1f2d31cd1d5c87d6c6b276
Alvin Coulson <aaacoulson@gmail.com>
2021-02-09 18:33:29 -0500

    refactor SignIn for device responsiveness

--- farm-connect-frontend/src/components/SignInForm/SignInForm.js
+++ farm-connect-frontend/src/components/SignInForm/SignInForm.js
@@ -8,14 +8,14 @@ const SignInForm = props => {
     return (
         <>
             <form id="signin_form" onSubmit={props.handleSubmit}>
-                <p><input id="signin-email" className="sign_up_in_input" type="text" name="email" placeholder="email" value={props.email} onChange={props.handleChange} /></p>
-                <p><input id="signin-password" className="sign_up_in_input" type="password" name="password" placeholder="password" value={props.password} onChange={props.handleChange} /></p>
+                <p><input id="signin-email" className="signin_inputs" type="text" name="email" placeholder="email" value={props.email} onChange={props.handleChange} /></p>
+                <p><input id="signin-password" className="signin_inputs" type="password" name="password" placeholder="password" value={props.password} onChange={props.handleChange} /></p>
                 <p id="errors_p">{props.errorMessages ? props.errorMessages : ""}</p>
                 <button id="signin_button" className="signin_button">Sign In</button>
             </form>
             <button className="signup_button" onClick={props.handleSwitchToSignUp}>Sign Up</button>
         </>
     )
-}
+};
 
 export default SignInForm;
