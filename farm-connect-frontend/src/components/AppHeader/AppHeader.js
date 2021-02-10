7073b8d24e6da7e1cf0e4a8097fbfd0d73f08541
Alvin Coulson <aaacoulson@gmail.com>
2021-02-10 14:06:26 -0500

    change the class names and ids on AppHeader

--- farm-connect-frontend/src/components/AppHeader/AppHeader.js
+++ farm-connect-frontend/src/components/AppHeader/AppHeader.js
@@ -4,9 +4,9 @@ import './AppHeader.css';
 
 const AppHeader = () => {
     return(
-        <div className="AppHeader_main_div">
-            <img id="logo_img" alt="Farm Connect Logo" src={logoSrc} />
-            <div id="name_div">farmConnect</div>
+        <div className="AppHeader-main-div">
+            <img id="logo-img" alt="Farm Connect Logo" src={logoSrc} />
+            <div id="name-div">farmConnect</div>
             <span>changing lives through connects</span>
         </div>
     )
