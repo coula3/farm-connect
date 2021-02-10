fa0d4754e28b2d59366e2e146285dc0fb9812167
Alvin Coulson <aaacoulson@gmail.com>
2021-02-10 14:40:08 -0500

    change ids and class names on HeaderNarBar and refactor to be responsive

--- farm-connect-frontend/src/components/HeaderNavBar/HeaderNavBar.js
+++ farm-connect-frontend/src/components/HeaderNavBar/HeaderNavBar.js
@@ -14,14 +14,14 @@ const HeadNavBar = (props) => {
     }
 
     return (
-        <div className="HeaderNavBar_main_div">
+        <div className="HeaderNavBar-main-div">
             { props.userPhoto
-                ?   <span className="header_img_span"><img className="header_img" src={userPhoto(props.userPhoto)} alt="user avatar" /></span>
-                :   <span className="header_img_span"><img className="header_img" src={avatar} alt="anonymous avatar" /></span>
+                ?   <span className="header-img-span"><img className="header-img" src={userPhoto(props.userPhoto)} alt="user avatar" /></span>
+                :   <span className="header-img-span"><img className="header-img" src={avatar} alt="anonymous avatar" /></span>
             }
-            <Link id="hnb_name_link" to={`/users/${props.userId}`} title="View Profile">{getFullName(props.userAttributes.first_name, props.userAttributes.last_name)}</Link>
-            <span id="user_type_global_span"><span id="user_type_span"><strong>{userType(props.userAttributes.type)}</strong></span></span>
-            <Link id="signout_link" to={paths().SIGNOUT_PATH} onClick={props.userSignOut}>Sign Out</Link>
+            <Link id="hnb-name-link" to={`/users/${props.userId}`} title="View Profile">{getFullName(props.userAttributes.first_name, props.userAttributes.last_name)}</Link>
+            <span id="user-type-global-span"><span id="user-type-span"><strong>{userType(props.userAttributes.type)}</strong></span></span>
+            <Link id="signout-link" to={paths().SIGNOUT_PATH} onClick={props.userSignOut}>Sign Out</Link>
         </div>
     )
 }
