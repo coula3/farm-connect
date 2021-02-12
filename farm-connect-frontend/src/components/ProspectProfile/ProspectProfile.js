97f3465dcf6c62ff4d476b843d3e3a73f84ab0f2
Alvin Coulson <aaacoulson@gmail.com>
2021-02-12 16:57:02 -0500

    refactor ProspectProfile to be responsive

--- farm-connect-frontend/src/components/ProspectProfile/ProspectProfile.js
+++ farm-connect-frontend/src/components/ProspectProfile/ProspectProfile.js
@@ -48,37 +48,36 @@ const ProspectProfile = (props) => {
     }
 
     return (
-        <div className="ProspectProfile_main_div">
+        <div className="ProspectProfile-main-div">
             { props.isLoadingProspect
                 ?   <Loader />
-                :   <div className="prospect_profile_card">
+                :   <div className="prospect-profile-card">
                         {(() => toFetchProspect = props.match.params.id !== props.prospect.id)()}
                         <h3>Prospect Profile</h3>
 
-                        <div id="img_div">
+                        <div className="img-div">
                             { props.prospect.attributes.image
-                                ?   <img className="prospect_img" src={prospectPhoto(props.prospect.attributes.image)} alt="user avatar" />
-                                :   <img className="prospect_img" src={avatar} alt="anonymous avatar" />
+                                ?   <img className="prospect-img" src={prospectPhoto(props.prospect.attributes.image)} alt="user avatar" />
+                                :   <img className="prospect-img" src={avatar} alt="anonymous avatar" />
                             }
                         </div>
 
                         <p><label><strong>UID:</strong> </label>{props.prospect.id}</p>
                         <p><label><strong>Name:</strong> </label>{getFullName(props.prospect.attributes.first_name, props.prospect.attributes.last_name)}</p>
                         <p><label><strong>Email:</strong> </label>{props.prospect.attributes.email.toLowerCase()}</p>
-                        <br />
-                        <p className="join_date_p"><label><strong>Joined:</strong> </label>{getDate(props.prospect.attributes.created_at)}</p>
+                        <p className="join-date-p"><label><strong>Joined:</strong> </label>{getDate(props.prospect.attributes.created_at)}</p>
 
                         { !isCurrentUser(props.prospect.id)
                             ?   !usersConnected
-                                ?   <button className="global_btn request_btn" onClick={(e) => connectUnconnectUsers(e, props.userId, props.prospect.id)}>Request Connect</button>
+                                ?   <button id="prospect-request-btn" className="global-btn" onClick={(e) => connectUnconnectUsers(e, props.userId, props.prospect.id)}>Request Connect</button>
                                     :    pendingAcceptance && !connectionByUser
                                         ?   <>
-                                                <button className="global_btn accept_btn" onClick={(e) => connectUnconnectUsers(e, props.userId, props.prospect.id)}>Accept</button>
-                                                <button className="global_btn decline_btn" onClick={(e) => connectUnconnectUsers(e, props.userId, props.prospect.id)}>Decline</button>
+                                                <button id="prospect-accept-btn" className="global-btn" onClick={(e) => connectUnconnectUsers(e, props.userId, props.prospect.id)}>Accept</button>
+                                                <button id="prospect-decline-btn" className="global-btn" onClick={(e) => connectUnconnectUsers(e, props.userId, props.prospect.id)}>Decline</button>
                                             </>
                                                 :   pendingAcceptance && connectionByUser
-                                                    ?   <button className="global_btn cancel_request_btn" onClick={(e) => connectUnconnectUsers(e, props.userId, props.prospect.id)}>Cancel Request</button>
-                                                        :   <div className="connect_btn_div"><button className="unconnect_btn" onClick={(e) => connectUnconnectUsers(e, props.userId, props.prospect.id)}>Unconnect</button></div>
+                                                    ?   <button id="prospect-cancel-request-btn" className="global-btn" onClick={(e) => connectUnconnectUsers(e, props.userId, props.prospect.id)}>Cancel Request</button>
+                                                        :   <div className="connect-btn-div"><button  id="prospect-unconnect-btn" onClick={(e) => connectUnconnectUsers(e, props.userId, props.prospect.id)}>Unconnect</button></div>
                             :   null
                         }
                     </div>
