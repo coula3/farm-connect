4a2612d000f26f542740ea1a7802748231c09e84
Alvin Coulson <aaacoulson@gmail.com>
2021-02-13 11:30:13 -0500

    refactor CreateListing to be devise responsive

--- farm-connect-frontend/src/containers/CreateListing/CreateListingForm.js
+++ farm-connect-frontend/src/containers/CreateListing/CreateListingForm.js
@@ -4,63 +4,42 @@ import * as messages from '../../utils/errorsUtils/listingErrors';
 const CreateListingForm = props => (
     <>
         <form onSubmit={props.handleSubmit}>
-            <table className="center">
-                <tbody>
-                    <tr>
-                        <td className="commodity_td">Commodity</td>
-                        <td>
-                            <select className="commodity_select" name="commodity" id="commodity-select" value={props.commodity} onChange={props.handleChange}>
-                                { props.commoditiesList.map((commodity, idx) =>
-                                    (<option key={idx} value={commodity}>{commodity}</option>))
-                                }
-                            </select>
-                            <br />
-                            <span className="p_errors errors_padding_left">{messages.commodityError(props.errorMessages)}</span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td className="listing_caption_td">Estimated Availability </td>
-                        <td>
-                            <input className="availability_input" name="availability" type="date" value={props.availability} onChange={props.handleChange} />
-                            <br />
-                            <span className="p_errors errors_padding_left">{messages.availabilityError(props.errorMessages)}</span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td className="listing_caption_td">Quantity </td>
-                        <td><input className="quantity_input" name="quantity" type="number" min="0" value={props.quantity} onChange={props.handleChange} onKeyUp={props.enableDisableMeasure} onMouseUp={props.enableDisableMeasure}/></td>
-                    </tr>
-                    <tr>
-                        <td className="listing_caption_td">Measure </td>
-                        <td>
-                            <select id="cl_measure_select" className="measure_select" name="measure" disabled={props.disableMeasure} value={props.measure} onChange={props.handleChange} >
-                                {props.measuresList.map((measure, idx) =>
-                                (<option key={idx} value={measure}>{measure}</option>))}
-                            </select>
-                            <br />
-                            <span className="p_errors">{messages.measureError(props.errorMessages)}</span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td className="listing_caption_td">Available </td>
-                        <td>
-                            <select className="available_select" name="available" id="availableSelect" value={props.available} onChange={props.handleChange}>
-                                <option value="No">No</option>
-                                <option value="Yes">Yes</option>
-                            </select>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td className="listing_caption_td">Supplementary Info </td>
-                        <td>
-                            <textarea className="info_textarea" name="information" id="information" rows="7" cols="42" maxLength={props.maxInfoCharacters} value={props.information} onChange={props.handleChange}></textarea>
-                            <br />
-                            <label style={props.maxXterColor}>{props.maxInfoCharacters - props.information.length}</label>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-            <p id="cl_btn_p"><input id="cl_btn" className="global_btn" type="submit" /></p>
+            <label className="create-listing-label">Commodity</label>
+            <select className="create-listing-commodity-select" name="commodity" id="commodity-select" value={props.commodity} onChange={props.handleChange}>
+                { props.commoditiesList.map((commodity, idx) =>
+                    (<option key={idx} value={commodity}>{commodity}</option>))
+                }
+            </select>
+            <span className="create-listing-errors-span">{messages.commodityError(props.errorMessages)}</span>
+
+            <label className="create-listing-label create-listing-label-margin">Estimated Availability </label>
+            <input className="create-listing-availability-input" name="availability" type="date" value={props.availability} onChange={props.handleChange} />
+            <span className="create-listing-errors-span">{messages.availabilityError(props.errorMessages)}</span>
+
+            <label className="create-listing-label create-listing-label-margin">Quantity </label><br />
+            <input className="create-listing-quantity-input" name="quantity" type="number" min="0" value={props.quantity} onChange={props.handleChange} onKeyUp={props.enableDisableMeasure} onMouseUp={props.enableDisableMeasure}/>
+
+            <select className="create-listing-measure-select" name="measure" disabled={props.disableMeasure} value={props.measure} onChange={props.handleChange} >
+                {props.measuresList.map((measure, idx) =>
+                (<option key={idx} value={measure}>{measure}</option>))}
+            </select>
+            <span className="create-listing-errors-span">{messages.measureError(props.errorMessages)}</span>
+
+            <label className="create-listing-label create-listing-label-margin">Available </label>
+            <select className="create-listing-available-select" name="available" id="availableSelect" value={props.available} onChange={props.handleChange}>
+                <option value="No">No</option>
+                <option value="Yes">Yes</option>
+            </select>
+
+            <label className="create-listing-label create-listing-label-margin">Supplementary Info </label>
+            <textarea className="create-listing-textarea" name="information" id="information" rows="8" maxLength={props.maxInfoCharacters} value={props.information} onChange={props.handleChange}></textarea>
+            <label className="create-listing-char-label" style={props.maxXterColor}>
+                {props.maxInfoCharacters - props.information.length}
+            </label>
+
+            <p id="create-listing-btn-p">
+                <input id="create-listing-btn" className="global-btn" type="submit" />
+            </p>
         </form>
     </>
 )
