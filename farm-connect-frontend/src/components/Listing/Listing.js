800fb3c10c3662f2c806d9eafae15809d815510b
Alvin Coulson <aaacoulson@gmail.com>
2021-02-13 12:41:18 -0500

    revise ids and class names on Listing

--- farm-connect-frontend/src/components/Listing/Listing.js
+++ farm-connect-frontend/src/components/Listing/Listing.js
@@ -17,7 +17,7 @@ const Listing = (props) => {
                 return (
                     <>
                         <br />
-                        <button className="global_btn listing_btn" onClick={() => props.history.push(`/listings/${listingId}/edit`)}>Edit</button>
+                        <button id="listing-edit-btn" className="global-btn" onClick={() => props.history.push(`/listings/${listingId}/edit`)}>Edit</button>
                     </>
                 );
             } else {
@@ -69,62 +69,95 @@ const Listing = (props) => {
     props.userAttributes.listings.length > 1 ? listingPluralized = "My Listings" : listingPluralized = "My Listing";
 
     return (
-        <div className="Listing_main_div">
+        <div className="Listing-main-div">
             { props.isLoading
                 ?   <Loader />
-                :   <div id="listing_card">
+                :   <div id="listing-card">
                         {(() => toFetchListing = props.match.params.id !== props.listing.id)()}
 
-                        <div id="listing_details">
+                        <div id="listing-details">
                             { props.listing.attributes.interests.length >= 5 && !props.listing.attributes.closed
-                                ?   <div id="interest_div">
+                                ?   <div id="interest-div">
                                         <strong>HIGH INTEREST</strong>
                                     </div>
                                 :   null
                             }
 
                             <h3>Listing</h3>
-                            <p><label className="color_lbl padding_lbl"><strong>LID:</strong> </label><span className="el_id_span">{padIds(props.listing.id)}</span></p>
-                            <p><label className="color_lbl padding_lbl"><strong>Listing Date:</strong> </label>{getDate(props.listing.attributes.date)}</p>
-                            <p><label className="color_lbl padding_lbl"><strong>Farmer:</strong> </label><Link id="name_link" to={`/farmers/${props.listing.attributes.user.id}`} onClick={(e) => handleFetchFarmer(e, props.listing.attributes.user.id)}>{getFullName(props.listing.attributes.user.first_name, props.listing.attributes.user.last_name)}</Link></p>
-                            <p><label className="color_lbl padding_lbl"><strong>Commodity:</strong> </label>{props.listing.attributes.commodity.name}</p>
-                            <p><label className="color_lbl padding_lbl"><strong>Estimated Availability:</strong> </label>{props.listing.attributes.availability ? getDate(props.listing.attributes.availability) : null}</p>
+
+                            <p>
+                                <label className="listing-label-color listing-label-padding"><strong>LID:</strong> </label>
+                                <span className="listing-id-span">{padIds(props.listing.id)}</span>
+                            </p>
+
+                            <p>
+                                <label className="listing-label-color listing-label-padding"><strong>Listing Date:</strong></label>
+                                {getDate(props.listing.attributes.date)}
+                            </p>
+
+                            <p>
+                                <label className="listing-label-color listing-label-padding"><strong>Farmer:</strong></label>
+                                <Link id="listing-name-link" to={`/farmers/${props.listing.attributes.user.id}`} onClick={(e) => handleFetchFarmer(e, props.listing.attributes.user.id)}>
+                                    {getFullName(props.listing.attributes.user.first_name, props.listing.attributes.user.last_name)}
+                                </Link>
+                            </p>
+
+                            <p>
+                                <label className="listing-label-color listing-label-padding"><strong>Commodity:</strong></label>
+                                {props.listing.attributes.commodity.name}
+                            </p>
+
+                            <p>
+                                <label className="listing-label-color listing-label-padding"><strong>Estimated Availability:</strong></label>
+                                {props.listing.attributes.availability ? getDate(props.listing.attributes.availability) : null}
+                            </p>
 
                             { props.listing.attributes.available
-                                ?   <p><label className="color_lbl padding_lbl"><strong>Available:</strong> </label>{convertTrueToYes(props.listing.attributes.available)}</p>
+                                ?   <p>
+                                        <label className="listing-label-color listing-label-padding"><strong>Available:</strong></label>
+                                        {convertTrueToYes(props.listing.attributes.available)}
+                                    </p>
                                 :   null
                             }
 
                             { props.listing.attributes.quantity
-                                ?   <p><label className="color_lbl padding_lbl"><strong>Quantity:</strong> </label>{props.listing.attributes.quantity} {props.listing.attributes.quantity > 1 ? props.listing.attributes.measure + "s" : props.listing.attributes.measure}</p>
+                                ?   <p>
+                                        <label className="listing-label-color listing-label-padding"><strong>Quantity:</strong> </label>
+                                        {props.listing.attributes.quantity} {props.listing.attributes.quantity > 1 ? props.listing.attributes.measure + "s" : props.listing.attributes.measure}
+                                    </p>
                                 :   null
                             }
 
                             { props.listing.attributes.information
                                 ?   <>
-                                        <label className="color_lbl"><strong>Supplementary Information</strong></label>
-                                        <p id="info_p">{props.listing.attributes.information}</p>
+                                        <label className="listing-label-color"><strong>Supplementary Information</strong></label>
+                                        <p id="listing-info-p">{props.listing.attributes.information}</p>
                                     </>
                                 :   null
                             }
 
-                            <p className="no_select"><label id="heart_lbl" style={{color: setHeartColor(props.userId, props.listing.attributes.interests)}} onClick={() => handleUpdateUserListingInterest(props.userId, props.listing.attributes.user.id, props.listing.id, props.listing.attributes.interests)}>{selectHeartType(props.userId, props.listing.attributes.interests)}</label>{props.listing.attributes.interests.length}</p>
+                            <p className="no-select">
+                                <label id="heart-lbl" style={{color: setHeartColor(props.userId, props.listing.attributes.interests)}} onClick={() => handleUpdateUserListingInterest(props.userId, props.listing.attributes.user.id, props.listing.id, props.listing.attributes.interests)}>
+                                    {selectHeartType(props.userId, props.listing.attributes.interests)}
+                                </label>
+                                {props.listing.attributes.interests.length}
+                            </p>
 
                             { props.listing.attributes.closed
-                                ?   <h5 id="closed_listing">Closed on {getDateTime(props.listing.attributes.closed)}</h5>
+                                ?   <h5 id="closed-listing">Closed on {getDateTime(props.listing.attributes.closed)}</h5>
                                 :   parseInt(props.userId) === props.listing.attributes.user.id
-                                    ?   <h5 id="listing_last_edit">Last edited on {getDateTime(props.listing.attributes.updated_at)}</h5>
+                                    ?   <h5 id="listing-last-edit">Last edited on {getDateTime(props.listing.attributes.updated_at)}</h5>
                                     :   null
                             }
 
                             {renderEditButton(props.userId, props.listing.attributes.user.id, props.listing.id)}
 
-                            <div style={{marginTop:"20px"}}>
+                            <div id="listing-btn-div">
                                 { props.userAttributes.type === "Farmer"
-                                    ?   <button className="global_btn listing_btn" onClick={handlePushToUserListings}>{listingPluralized}</button>
+                                    ?   <button id="listing-mylisting-btn" className="global-btn" onClick={handlePushToUserListings}>{listingPluralized}</button>
                                     :   null
                                 }
-                                <button className="global_btn listing_btn" onClick={handlePushToListings}>Listings</button>
+                                <button id="listing-listings-btn" className="global-btn" onClick={handlePushToListings}>Listings</button>
                             </div>
                         </div>
                     </div>
