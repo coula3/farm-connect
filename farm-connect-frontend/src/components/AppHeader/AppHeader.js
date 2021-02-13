410495a78d13e72d930f40cab4ec75353c0f3144
Alvin Coulson <aaacoulson@gmail.com>
2021-02-12 22:07:01 -0500

    add link to app logo on AppHeader

--- farm-connect-frontend/src/components/AppHeader/AppHeader.js
+++ farm-connect-frontend/src/components/AppHeader/AppHeader.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import logoSrc from '../../assets/farmConnectLogo.png'
+import logoSrc from '../../assets/farmConnectLogo.png';
+import { Link } from 'react-router-dom';
 import './AppHeader.css';
 
 const AppHeader = () => {
     return(
         <div className="AppHeader-main-div">
-            <img id="logo-img" alt="Farm Connect Logo" src={logoSrc} />
+            <Link to="/listings">
+                <img id="logo-img" alt="Farm Connect Logo" src={logoSrc} />
+            </Link>
             <div id="name-div">farmConnect</div>
             <span>changing lives through connects</span>
         </div>
