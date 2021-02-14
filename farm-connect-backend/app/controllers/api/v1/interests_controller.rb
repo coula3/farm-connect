7ceaa2aa283503eb3a7e2fd3d890761ce420b23b
Alvin Coulson <aaacoulson@gmail.com>
2021-02-14 10:04:00 -0500

    rename block variable in index action on InterestsController

--- farm-connect-backend/app/controllers/api/v1/interests_controller.rb
+++ farm-connect-backend/app/controllers/api/v1/interests_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::InterestsController < ApplicationController
         open_listing_ids_with_interests =  Interest.select {|interest| !interest.listing.closed }.pluck(:listing_id)
 
         listing_interests_hash = Hash.new(0)
-        open_listing_ids_with_interests.each {|e| listing_interests_hash[e] += 1}
+        open_listing_ids_with_interests.each {|id| listing_interests_hash[id] += 1}
 
         listing_interests_array = listing_interests_hash.sort_by {|k, v| v}.reverse.slice(0..4)
         listings_commodity_names = listing_interests_array.map {|listing| Listing.find(listing[0]).commodity.name}
