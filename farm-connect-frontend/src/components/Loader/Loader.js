71df49485bd30d93a69ff034887920f17da53791
Alvin Coulson <aaacoulson@gmail.com>
2021-02-18 16:46:18 -0500

    revise id and class name on Loader

--- farm-connect-frontend/src/components/Loader/Loader.js
+++ farm-connect-frontend/src/components/Loader/Loader.js
@@ -3,8 +3,8 @@ import loaderSrc from '../../assets/loader.gif'
 import './Loader.css';
 
 const Loader = () => (
-    <div className="Loader_main_div">
-        <img alt="loader" src={loaderSrc} id="img_width" />
+    <div className="Loader-main-div">
+        <img alt="loader" src={loaderSrc} id="loader-img" />
     </div>
 )
 
