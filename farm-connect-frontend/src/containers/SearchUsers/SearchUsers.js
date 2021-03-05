3b007116634fa4823d3fb7df78385d417cb1850f
Alvin Coulson <aaacoulson@gmail.com>
2021-03-05 13:55:48 -0500

    remove search parameter on click of submit btn during network disconnection

--- farm-connect-frontend/src/containers/SearchUsers/SearchUsers.js
+++ farm-connect-frontend/src/containers/SearchUsers/SearchUsers.js
@@ -32,6 +32,7 @@ class SearchUsers extends Component {
             });
         } else {
             this.props.searchResults.data && this.resetSearchForm();
+            this.props.location.search && window.history.pushState({}, document.title, `${this.props.location.pathname}`);
         }
     }
 
