8724f79b7f5686cbb35433d93183a78a28237dec
Alvin Coulson <aaacoulson@gmail.com>
2021-03-14 18:00:14 -0400

    ref last commit

--- farm-connect-frontend/src/components/NoMatch/NoMatch.js
+++ farm-connect-frontend/src/components/NoMatch/NoMatch.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { withRouter } from 'react-router-dom';
 import sadEmoji from '../../assets/sadEmoji.png';
 import './NoMatch.css';
